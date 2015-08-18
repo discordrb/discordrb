5ce0491ee634377813f571c9e33458686afadf2d
meew0 <blactbt@live.de>
2015-08-18 20:06:31 +0200

    Add basic websocket logic

--- lib/discordrb/bot.rb
+++ lib/discordrb/bot.rb
@@ -1,4 +1,6 @@
 require 'rest-client'
+require 'faye/websocket'
+require 'eventmachine'
 
 require 'discordrb/endpoints/endpoints'
 
@@ -10,15 +12,45 @@ module Discordrb
       @email = email
       @password = password
 
+      @token = login()
+      websocket_connect()
+    end
+
+    private
+
+    def login
       # Login
-      login_response = RestClient.post Discordrb::Endpoints::LOGIN, :email => email, :password => password
+      login_response = RestClient.post Discordrb::Endpoints::LOGIN, :email => @email, :password => @password
       raise HTTPStatusException.new(response.code) if response.code >= 400
 
       # Parse response
       login_response_object = JSON.parse(login_response)
       raise InvalidAuthenticationException unless login_response_object[token]
 
-      @token = login_response_object[token]
+      login_response_object[token]
+    end
+
+    def websocket_connect
+      EM.run {
+        @ws = Faye::WebSocket::Client.new(Discordrb::Endpoints::WEBSOCKET_HUB)
+
+        @ws.on :open do |event|; websocket_message(event); end
+        @ws.on :message do |event|; websocket_message(event); end
+
+        @ws.on :close do |event|
+          websocket_close(event)
+          @ws = nil
+        end
+      }
+    end
+
+    def websocket_message(event)
+    end
+
+    def websocket_close(event)
+    end
+
+    def websocket_open(event)
     end
   end
 end
