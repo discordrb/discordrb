854a5de3ca7be967a7430bb6775f181e586ca9f0
meew0 <blactbt@live.de>
2015-08-19 09:24:49 +0200

    Add a class to provide data for message events

---
+++ lib/discordrb/events/message.rb
@@ -0,0 +1,15 @@
+module Discordrb
+  class MessageEvent
+    attr_reader :message
+
+    def initialize(message)
+      @message = message
+    end
+
+    def author; @message.author; end
+    def user; @message.author; end
+    def channel; @message.channel; end
+    def content; @message.content; end
+    def text; @message.content; end
+  end
+end
