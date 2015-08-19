34d27b95abd63a12fbe3b93d88fcf44adbfdd932
meew0 <blactbt@live.de>
2015-08-19 11:20:30 +0200

    More restructuring

--- lib/discordrb/events/utility.rb
+++ lib/discordrb/events/utility.rb
@@ -1,11 +1,13 @@
-module Discordrb::Events::Utility
-  def matches_all(attributes, to_check, &block)
-    # First case: there's only a single attribute
-    unless attributes.is_a? Array
-      return true if yield(attributes, to_check)
-    end
+module Discordrb::Events
+  module Utility
+    def matches_all(attributes, to_check, &block)
+      # First case: there's only a single attribute
+      unless attributes.is_a? Array
+        return true if yield(attributes, to_check)
+      end
 
-    # Second case: it's an array of attributes
-    attributes.reduce(false) { |result, element| result || yield(element, to_check) }
+      # Second case: it's an array of attributes
+      attributes.reduce(false) { |result, element| result || yield(element, to_check) }
+    end
   end
 end
