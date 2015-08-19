8bbf1ad5c2d9b26eefee7bb2033014e542da08ec
meew0 <blactbt@live.de>
2015-08-19 09:40:07 +0200

    Add a utility function for event matching

---
+++ lib/discordrb/events/utility.rb
@@ -0,0 +1,11 @@
+module Discordrb::Events
+  def matches_all(attributes, to_check, &block)
+    # First case: there's only a single attribute
+    unless attributes.is_a? Array
+      return true if yield(attributes, to_check)
+    end
+
+    # Second case: it's an array of attributes
+    attributes.reduce(false) { |result, element| result || yield(element, to_check) }
+  end
+end
