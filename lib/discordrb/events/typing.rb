270a7c5a779a279da5d07d34063938aafc1d37a3
meew0 <blactbt@live.de>
2015-08-19 13:46:01 +0200

    Add an event handler for TYPING_START

---
+++ lib/discordrb/events/typing.rb
@@ -0,0 +1,42 @@
+require 'discordrb/events/generic'
+
+module Discordrb::Events
+  class TypingEvent
+    attr_reader :channel, :timestamp
+
+    def initialize(data, bot)
+      @user_id = data['user_id']
+      @channel_id = data['user_id']
+      @channel = bot.channel(@channel_id)
+      @timestamp = Time.at(data['timestamp'].to_i)
+    end
+  end
+
+  class TypingEventHandler < EventHandler
+    def matches?(event)
+      # Check for the proper event type
+      return false unless event.is_a? TypingEvent
+
+      return [
+        matches_all(@attributes[:in], event.channel) do |a,e|
+          if a.is_a? String
+            a == e.name
+          elsif a.is_a? Fixnum
+            a == e.id
+          else
+            a == e
+          end
+        end,
+        matches_all(@attributes[:from], event.user) do |a,e|
+          if a.is_a? String
+            a == e.name
+          elsif a.is_a? Fixnum
+            a == e.id
+          else
+            a == e
+          end
+        end
+      ].reduce(true, &:&)
+    end
+  end
+end
