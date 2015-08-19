dd085e78db9d681ddd3532c7995691c210ac985b
meew0 <blactbt@live.de>
2015-08-19 11:23:03 +0200

    Add a nil check to matches_all

--- lib/discordrb/events/utility.rb
+++ lib/discordrb/events/utility.rb
@@ -1,6 +1,9 @@
 module Discordrb::Events
   module Utility
     def matches_all(attributes, to_check, &block)
+      # "Zeroth" case: attributes is nil
+      return false unless attributes
+      
       # First case: there's only a single attribute
       unless attributes.is_a? Array
         return true if yield(attributes, to_check)
