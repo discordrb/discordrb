266db531637abc8399af3ec4a8fd3f2a7d624575
meew0 <blactbt@live.de>
2015-08-19 11:57:58 +0200

    Restructure message sending request

--- lib/discordrb/bot.rb
+++ lib/discordrb/bot.rb
@@ -40,7 +40,7 @@ module Discordrb
         'mentions' => []
       }
 
-      RestClient.post Discordrb::Endpoints::CHANNELS + "/#{channel_id}/messages", data.to_json, :content_type => :json, {:Authorization => @token}
+      RestClient.post Discordrb::Endpoints::CHANNELS + "/#{channel_id}/messages", data.to_json, {:Authorization => @token, :content_type => :json}
     end
 
     def message(attributes = {}, &block)
