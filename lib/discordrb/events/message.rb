b0ae2487912fec4bdd8fa6c9258ee5951241d06d
meew0 <blactbt@live.de>
2015-08-19 10:00:53 +0200

    Add matching logic for MessageEventHandler

--- lib/discordrb/events/message.rb
+++ lib/discordrb/events/message.rb
@@ -20,6 +20,33 @@ module Discordrb::Events
     end
 
     def match(event)
+      # Check for the proper event type
+      return false unless event.is_a? MessageEvent
+
+      return [
+        matches_all(@attributes[:starting_with], event.content) { |a,e| e.start_with? a },
+        matches_all(@attributes[:ending_with], event.content) { |a,e| e.end_with? a },
+        matches_all(@attributes[:containing], event.content) { |a,e| e.include? a },
+        matches_all(@attributes[:in], event.channel) do |a,e|
+          if a.is_a? String
+            a == e.name
+          elsif a.is_a? Fixnum
+            a == e.id
+          else
+            a == e
+          end
+        end,
+        matches_all(@attributes[:from], event.author) do |a,e|
+          if a.is_a? String
+            a == e.name
+          elsif a.is_a? Fixnum
+            a == e.id
+          else
+            a == e
+          end
+        end,
+        matches_all(@attributes[:with_text], event.content) { |a,e| e == a }
+      ].reduce(false, &:|)
     end
   end
 end
