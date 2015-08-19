5c2867abf645f48e69057806881a2869ca7994ae
meew0 <blactbt@live.de>
2015-08-19 10:15:34 +0200

    Add class to represent Discord users

--- lib/discordrb/data.rb
+++ lib/discordrb/data.rb
@@ -1,6 +1,18 @@
 # These classes hold relevant Discord data, such as messages or channels.
 
 module Discordrb
+  class User
+    attr_reader :username, :id, :discriminator, :avatar
+    alias_method :name, :username
+
+    def initialize(data)
+      @username = data['username']
+      @id = data['id'].to_i
+      @discriminator = data['discriminator']
+      @avatar = data['avatar']
+    end
+  end
+
   class Message
     attr_reader :content, :author, :channel, :timestamp, :id, :mentions
     alias_method :user, :author
