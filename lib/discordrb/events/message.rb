dea54a8b476290bb9645efb183a6e2d99b5c1c79
meew0 <blactbt@live.de>
2015-08-23 11:23:48 +0200

    Remove duplicate "message" method (thanks GabeMachida on Discord)

--- lib/discordrb/events/message.rb
+++ lib/discordrb/events/message.rb
@@ -18,7 +18,6 @@ module Discordrb::Events
 
     def send_message(content); @message.channel.send_message(content); end
     alias_method :send, :send_message
-    alias_method :message, :send_message
     alias_method :respond, :send_message
   end
 
