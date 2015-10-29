594966d5bb6e0adf95d7170e4db7d530b8551140
meew0 <blactbt@live.de>
2015-10-29 20:48:12 +0100

    Wrap CommandBot, Command and CommandChain inside a module

--- lib/discordrb/commands/command-bot.rb
+++ lib/discordrb/commands/command-bot.rb
@@ -3,57 +3,59 @@ require 'discordrb/commands/parser'
 
 # Specialized bot to run commands
 
-class CommandBot < Bot
-  attr_reader :attributes, :prefix
+module Discordrb::Commands
+  class CommandBot < Bot
+    attr_reader :attributes, :prefix
 
-  def initialize(email, password, prefix, attributes = {}, debug = false)
-    super(email, password, debug)
-    @prefix = prefix
-    @commands = {}
-    @attributes = {
-      # Whether advanced functionality such as command chains are enabled
-      advanced_functionality: attributes[:advanced_functionality].nil? ? true : attributes[:advanced_functionality],
+    def initialize(email, password, prefix, attributes = {}, debug = false)
+      super(email, password, debug)
+      @prefix = prefix
+      @commands = {}
+      @attributes = {
+        # Whether advanced functionality such as command chains are enabled
+        advanced_functionality: attributes[:advanced_functionality].nil? ? true : attributes[:advanced_functionality],
 
-      # All of the following need to be one character
-      # String to designate previous result in command chain
-      previous: attributes[:previous] || '~',
+        # All of the following need to be one character
+        # String to designate previous result in command chain
+        previous: attributes[:previous] || '~',
 
-      # Command chain delimiter
-      chain_delimiter: attributes[:chain_delimiter] || '>',
+        # Command chain delimiter
+        chain_delimiter: attributes[:chain_delimiter] || '>',
 
-      # Chain argument delimiter
-      chain_args_delim: attributes[:chain_args_delim] || ':',
+        # Chain argument delimiter
+        chain_args_delim: attributes[:chain_args_delim] || ':',
 
-      # Sub-chain starting character
-      sub_chain_start: attributes[:sub_chain_start] || '[',
+        # Sub-chain starting character
+        sub_chain_start: attributes[:sub_chain_start] || '[',
 
-      # Sub-chain ending character
-      sub_chain_end: attributes[:sub_chain_end] || ']',
+        # Sub-chain ending character
+        sub_chain_end: attributes[:sub_chain_end] || ']',
 
-      # Quoted mode starting character
-      quote_start: attributes[:quote_start] || "'",
+        # Quoted mode starting character
+        quote_start: attributes[:quote_start] || "'",
 
-      # Quoted mode ending character
-      quote_end: attributes[:quote_end] || "'"
-    }
-  end
+        # Quoted mode ending character
+        quote_end: attributes[:quote_end] || "'"
+      }
+    end
 
-  def command(name, attributes = {}, &block)
-    @commands[name] = Command.new(name, attributes, &block)
-  end
+    def command(name, attributes = {}, &block)
+      @commands[name] = Command.new(name, attributes, &block)
+    end
 
-  def execute_command(name, event, arguments)
-    command = @commands[name]
-    command.call(event, arguments)
-  end
+    def execute_command(name, event, arguments)
+      command = @commands[name]
+      command.call(event, arguments)
+    end
 
-  def create_message(data)
-    message = Message.new(data, self)
-    event = MessageEvent.new(message, self)
+    def create_message(data)
+      message = Message.new(data, self)
+      event = MessageEvent.new(message, self)
 
-    if message.content.start_with? @prefix
-      chain = message.content[@prefix.length..-1]
-      CommandChain.new(chain, self, true).execute(event)
+      if message.content.start_with? @prefix
+        chain = message.content[@prefix.length..-1]
+        CommandChain.new(chain, self, true).execute(event)
+      end
     end
   end
 end
--- lib/discordrb/commands/parser.rb
+++ lib/discordrb/commands/parser.rb
@@ -1,183 +1,185 @@
-class Command
-  def initialize(name, attributes = {}, &block)
-    @name = name
-    @attributes = {
-      # The lowest permission level that can use the command
-      permission_level: attributes[:permission_level] || 0,
+module Discordrb::Commands
+  class Command
+    def initialize(name, attributes = {}, &block)
+      @name = name
+      @attributes = {
+        # The lowest permission level that can use the command
+        permission_level: attributes[:permission_level] || 0,
 
-      # Whether this command is usable in a command chain
-      chain_usable: attributes[:chain_usable].nil? ? true : attributes[:chain_usable],
+        # Whether this command is usable in a command chain
+        chain_usable: attributes[:chain_usable].nil? ? true : attributes[:chain_usable],
 
-      # Description (for help command)
-      description: attributes[:description] || '',
+        # Description (for help command)
+        description: attributes[:description] || '',
 
-      # Usage description (for help command and error messages)
-      usage: attributes[:usage] || '',
+        # Usage description (for help command and error messages)
+        usage: attributes[:usage] || '',
 
-      # Minimum number of arguments
-      min_args: attributes[:min_args] || 0,
+        # Minimum number of arguments
+        min_args: attributes[:min_args] || 0,
 
-      # Maximum number of arguments (-1 for no limit)
-      max_args: attributes[:max_args] || -1
-    }
+        # Maximum number of arguments (-1 for no limit)
+        max_args: attributes[:max_args] || -1
+      }
 
-    @block = block
-  end
-
-  def call(event, arguments)
-    @block.call(event, *arguments)
-  end
-end
-
-class CommandChain
-  def initialize(chain, bot, first_chain = false)
-    @attributes = bot.attributes
-    # Append a chain delimiter to the first chain to make it easier on the parser (hacky!)
-    @chain = first_chain ? @attributes[:chain_delimiter] + chain : chain
-    @bot = bot
-  end
-
-  def execute_bare(event)
-    b_start, b_level = -1, 0
-    result = ''
-    quoted = false
-    hacky_delim, hacky_space, hacky_prev = [0xe001, 0xe002, 0xe003].map &:chr
-
-    @chain.each_char.each_with_index do |char, index|
-      # Quote begin
-      if char == @attributes[:quote_start] && !quoted
-        quoted = true
-        next
-      end
-
-      # Quote end
-      if char == @attributes[:quote_end] && quoted
-        quoted = false
-        next
-      end
-
-      if char == @attributes[:chain_delimiter] && quoted
-        result += hacky_delim
-        next
-      end
-
-      if char == @attributes[:previous] && quoted
-        result += hacky_prev
-        next
-      end
-
-      if char == ' ' && quoted
-        result += hacky_space
-        next
-      end
-
-      if char == @attributes[:sub_chain_start] && !quoted
-        b_start = index if b_level == 0
-        b_level += 1
-      end
-
-      result << char if b_level <= 0
-
-      if char == @attributes[:sub_chain_end] && !quoted
-        b_level -= 1
-        if b_level == 0
-          nested = @chain[b_start + 1 .. i - 1]
-          subchain = CommandChain.new(nested)
-          result << subchain.execute(event)
-        end
-      end
+      @block = block
     end
 
-    event.respond("Your subchains are mismatched! Make sure you don't have any extra #{@attributes[:sub_chain_start]}'s or #{@attributes[:sub_chain_end]}'s") unless b_level == 0
-
-    @chain = result
-
-    @chain_args, @chain = divide_chain(@chain)
-
-    prev = ''
-
-    chain_to_split = @chain
-
-    # Don't break if a command is called the same thing as the chain delimiter
-    chain_to_split.slice!(1..-1) if chain_to_split.start_with?(@attributes[:chain_delimiter])
-
-    first = true
-    chain_to_split.split(@attributes[:chain_delimiter]).each do |command|
-      command = @attributes[:chain_delimiter] + command if first && @chain.start_with?(@attributes[:chain_delimiter])
-      first = false
-
-      command.trim!
-
-      # Replace the hacky delimiter that was used inside quotes with actual delimiters
-      command.gsub! hacky_delim, @attributes[:chain_delimiter]
-
-      first_space = command.index ' '
-      command_name = first_space ? command[0..first_space-1] : command
-      arguments = first_space ? command[first_space+1..-1] : ''
-
-      # Append a previous sign if none is present
-      arguments << @attributes[:previous] unless arguments.include? @attributes[:previous]
-      arguments.gsub! @attributes[:previous], prev
-
-      # Replace hacky previous signs with actual ones
-      arguments.gsub! hacky_prev, @attributes[:previous]
-
-      arguments.split! ' '
-
-      # Replace the hacky spaces with actual spaces
-      arguments.map! do |elem|
-        elem.gsub hacky_space, ' '
-      end
-
-      # Finally execute the command
-      prev = @bot.execute_command(command_name.to_sym, event, arguments)
+    def call(event, arguments)
+      @block.call(event, *arguments)
     end
-
-    prev
   end
 
-  def execute(event)
-    old_chain = @chain
-    result = execute_bare(event)
+  class CommandChain
+    def initialize(chain, bot, first_chain = false)
+      @attributes = bot.attributes
+      # Append a chain delimiter to the first chain to make it easier on the parser (hacky!)
+      @chain = first_chain ? @attributes[:chain_delimiter] + chain : chain
+      @bot = bot
+    end
 
-    @chain_args ||= []
+    def execute_bare(event)
+      b_start, b_level = -1, 0
+      result = ''
+      quoted = false
+      hacky_delim, hacky_space, hacky_prev = [0xe001, 0xe002, 0xe003].map &:chr
 
-    @chain_args.each do |arg|
-      case arg.first
-      when 'repeat'
-        new_result = ''
-        executed_chain = divide_chain(old_chain).last
-
-        arg[1].to_i.times do
-          new_result << CommandChain.new(executed_chain, @bot, true).execute
+      @chain.each_char.each_with_index do |char, index|
+        # Quote begin
+        if char == @attributes[:quote_start] && !quoted
+          quoted = true
+          next
         end
 
-        result = new_result
-      # TODO: more chain arguments
+        # Quote end
+        if char == @attributes[:quote_end] && quoted
+          quoted = false
+          next
+        end
+
+        if char == @attributes[:chain_delimiter] && quoted
+          result += hacky_delim
+          next
+        end
+
+        if char == @attributes[:previous] && quoted
+          result += hacky_prev
+          next
+        end
+
+        if char == ' ' && quoted
+          result += hacky_space
+          next
+        end
+
+        if char == @attributes[:sub_chain_start] && !quoted
+          b_start = index if b_level == 0
+          b_level += 1
+        end
+
+        result << char if b_level <= 0
+
+        if char == @attributes[:sub_chain_end] && !quoted
+          b_level -= 1
+          if b_level == 0
+            nested = @chain[b_start + 1 .. i - 1]
+            subchain = CommandChain.new(nested)
+            result << subchain.execute(event)
+          end
+        end
       end
+
+      event.respond("Your subchains are mismatched! Make sure you don't have any extra #{@attributes[:sub_chain_start]}'s or #{@attributes[:sub_chain_end]}'s") unless b_level == 0
+
+      @chain = result
+
+      @chain_args, @chain = divide_chain(@chain)
+
+      prev = ''
+
+      chain_to_split = @chain
+
+      # Don't break if a command is called the same thing as the chain delimiter
+      chain_to_split.slice!(1..-1) if chain_to_split.start_with?(@attributes[:chain_delimiter])
+
+      first = true
+      chain_to_split.split(@attributes[:chain_delimiter]).each do |command|
+        command = @attributes[:chain_delimiter] + command if first && @chain.start_with?(@attributes[:chain_delimiter])
+        first = false
+
+        command.trim!
+
+        # Replace the hacky delimiter that was used inside quotes with actual delimiters
+        command.gsub! hacky_delim, @attributes[:chain_delimiter]
+
+        first_space = command.index ' '
+        command_name = first_space ? command[0..first_space-1] : command
+        arguments = first_space ? command[first_space+1..-1] : ''
+
+        # Append a previous sign if none is present
+        arguments << @attributes[:previous] unless arguments.include? @attributes[:previous]
+        arguments.gsub! @attributes[:previous], prev
+
+        # Replace hacky previous signs with actual ones
+        arguments.gsub! hacky_prev, @attributes[:previous]
+
+        arguments.split! ' '
+
+        # Replace the hacky spaces with actual spaces
+        arguments.map! do |elem|
+          elem.gsub hacky_space, ' '
+        end
+
+        # Finally execute the command
+        prev = @bot.execute_command(command_name.to_sym, event, arguments)
+      end
+
+      prev
     end
 
-    result
-  end
+    def execute(event)
+      old_chain = @chain
+      result = execute_bare(event)
 
-  private
+      @chain_args ||= []
 
-  def divide_chain(chain)
-    chain_args_index = chain.index @attributes[:chain_args_delim]
-    chain_args = []
+      @chain_args.each do |arg|
+        case arg.first
+        when 'repeat'
+          new_result = ''
+          executed_chain = divide_chain(old_chain).last
 
-    if chain_args_index
-      chain_args = chain[0..chain_args_index].split ','
+          arg[1].to_i.times do
+            new_result << CommandChain.new(executed_chain, @bot, true).execute
+          end
 
-      # Split up the arguments
-
-      chain_args.map! do |arg|
-        arg.split ' '
+          result = new_result
+        # TODO: more chain arguments
+        end
       end
 
-      chain.slice!(chain_args_index+1..-1)
+      result
     end
 
-    [chain_args, chain]
+    private
+
+    def divide_chain(chain)
+      chain_args_index = chain.index @attributes[:chain_args_delim]
+      chain_args = []
+
+      if chain_args_index
+        chain_args = chain[0..chain_args_index].split ','
+
+        # Split up the arguments
+
+        chain_args.map! do |arg|
+          arg.split ' '
+        end
+
+        chain.slice!(chain_args_index+1..-1)
+      end
+
+      [chain_args, chain]
+    end
   end
 end
