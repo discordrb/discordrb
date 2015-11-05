347273728968b1307afd459eaa1148065ad979c0
meew0 <blactbt@live.de>
2015-11-05 20:58:55 +0100

    Create the message with the bot as the argument

--- lib/discordrb/bot.rb
+++ lib/discordrb/bot.rb
@@ -110,7 +110,7 @@ module Discordrb
         'mentions' => []
       }
       response = API.send_message(@token, channel_id, content)
-      Message.new(response)
+      Message.new(response, self)
     end
 
     def send_file(channel_id, file)
