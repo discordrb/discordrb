70ae38ddedc2ee787ef7a8c0bd467615bf87e9b7
meew0 <blactbt@live.de>
2015-11-09 17:40:50 +0100

    Fix the segfaults (improper frame size)

--- lib/discordrb/voice/encoder.rb
+++ lib/discordrb/voice/encoder.rb
@@ -5,15 +5,24 @@ module Discordrb::Voice
   # Wrapper class around opus-ruby
   class Encoder
     def initialize
-      @opus = Opus::Encoder.new(48_000, 1920, 1)
+      @sample_rate = 48_000
+      @frame_size = 960
+      # Mono because Discord would make it mono on playback anyway
+      @channels = 1
     end
 
     def encode(buffer)
-      @opus.encode(buffer, 1920)
+      # Create a new buffer each time so opus doesn't segfault. Don't ask me why
+      # It still segfaults though
+      opus = Opus::Encoder.new(@sample_rate, @frame_size, @channels)
+      encoded = opus.encode(buffer, 1920)
+      opus.destroy
+      encoded
     end
 
     def encode_file(file)
       command = "ffmpeg -i #{file.path} -f s16le -ar 48000 -ac 1 -af volume=1 pipe:1"
+      puts "Command: #{command}"
       IO.popen(command)
     end
   end
