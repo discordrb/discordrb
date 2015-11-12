5fb52e269de21d2e79c7a07afba2689867dbee3c
meew0 <blactbt@live.de>
2015-11-12 23:16:41 +0100

    Get rid of levenshtein (it was way too slow)

--- lib/discordrb.rb
+++ lib/discordrb.rb
@@ -4,16 +4,4 @@ require 'discordrb/commands/command_bot'
 
 # All discordrb functionality, to be extended by other files
 module Discordrb
-  module_function
-
-  # Simple Levenshtein distance (slow)
-  def levenshtein(one, two)
-    return 0 if one == two
-    return two.size if one.size == 0
-    return one.size if two.size == 0
-
-    [levenshtein(one.chop, two) + 1,
-     levenshtein(one, two.chop) + 1,
-     levenshtein(one.chop, two.chop) + (one[-1, 1] == two[-1, 1] ? 0 : 1)].min
-  end
 end
