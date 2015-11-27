7cc6b65e691cb1dc2006fa5f32eab8954872387d
meew0 <blactbt@live.de>
2015-11-27 22:10:13 +0100

    ...............

--- lib/discordrb/await.rb
+++ lib/discordrb/await.rb
@@ -15,7 +15,7 @@ module Discordrb
       dummy_handler = @bot.handler_class(@type).new(@attributes, @bot)
       return [nil, nil] unless dummy_handler.matches?(event)
 
-      should_delete = true if @block.call(event) != false || !block
+      should_delete = true if (@block && @block.call(event) != false) || !block
 
       [@key, should_delete]
     end
