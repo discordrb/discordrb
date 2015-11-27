13cea9edc3d24b7ffc9b335dc0120f6a88f0760d
meew0 <blactbt@live.de>
2015-11-27 17:30:23 +0100

    Add the await class itself

---
+++ lib/discordrb/await.rb
@@ -0,0 +1,22 @@
+module Discordrb
+  # Class that represents an await to wait for a further event
+  class Await
+    attr_reader :key, :type, :attributes
+
+    def initialize(bot, key, type, attributes, block = nil)
+      @bot = bot
+      @key = key
+      @type = type
+      @attributes = attributes
+      @block = block
+    end
+
+    def match(event)
+      dummy_handler = bot.handler_class(@type).new(@attributes) {}
+      return nil unless dummy_handler.matches?(event)
+
+      @block.call(event) if @block
+      @key
+    end
+  end
+end
