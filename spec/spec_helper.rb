8448776a239bc22cfe06913705a5112f2b7f4784
meew0 <blactbt@live.de>
2015-11-28 15:46:51 +0100

    Enable both syntaxes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,7 +28,7 @@ RSpec.configure do |config|
     #     # => "be bigger than 2 and smaller than 4"
     # ...rather than:
     #     # => "be bigger than 2"
-    expectations.syntax = :should
+    expectations.syntax = [:should, :expect]
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
