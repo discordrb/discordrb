79e13a54c920a4435bd2fd022990e6c37af0bee5
meew0 <blactbt@live.de>
2015-12-02 22:51:00 +0100

    Parse JSON when making an invite

--- lib/discordrb/data.rb
+++ lib/discordrb/data.rb
@@ -272,7 +272,7 @@ module Discordrb
 
     def make_invite(max_age = 0, max_uses = 0, temporary = false, xkcd = false)
       response = API.create_invite(@bot.token, @id, max_age, max_uses, temporary, xkcd)
-      Invite.new(response, @bot)
+      Invite.new(JSON.parse(response), @bot)
     end
 
     alias_method :send, :send_message
