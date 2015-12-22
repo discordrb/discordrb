4870162a0f0dd70ad1d8bb9a7ae29bae581527e6
meew0 <blactbt@live.de>
2015-12-22 23:52:30 +0100

    Go back to the old method of using mentions (<@id>) It has been announced that discord will get rid of the mentions array, and this way works again.

--- lib/discordrb/bot.rb
+++ lib/discordrb/bot.rb
@@ -277,19 +277,7 @@ module Discordrb
     def send_message(channel_id, content)
       debug("Sending message to #{channel_id} with content '#{content}'")
 
-      # Replace mentions
-      mentions = []
-      content.gsub!(/<@([0-9]+)>/) do
-        id = Regexp.last_match(1).to_i
-        if @users[id]
-          mentions << id
-          "@#{@users[id].name}"
-        else
-          "<@#{id}}>"
-        end
-      end
-
-      response = API.send_message(@token, channel_id, content, mentions)
+      response = API.send_message(@token, channel_id, content)
       Message.new(JSON.parse(response), self)
     end
 
