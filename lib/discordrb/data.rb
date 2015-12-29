0b45e1cbf0df83a2d01b41a90cd5f74b7fcc7c51
meew0 <blactbt@live.de>
2015-12-29 20:38:49 +0100

    This is not JS...

--- lib/discordrb/data.rb
+++ lib/discordrb/data.rb
@@ -435,7 +435,7 @@ module Discordrb
     def initialize(data, bot)
       @bot = bot
       @content = data['content']
-      @author = bot.user(data['author'].id.to_i)
+      @author = bot.user(data['author']['id'].to_i)
       @channel = bot.channel(data['channel_id'].to_i)
       @timestamp = Time.parse(data['timestamp'])
       @id = data['id'].to_i
