31b4ee73b2ec0e2a8a5793f07793d93ec431c03d
meew0 <blactbt@live.de>
2016-01-03 14:41:31 +0100

    Add a PlayingEvent for when a user plays a game

--- lib/discordrb/events/presence.rb
+++ lib/discordrb/events/presence.rb
@@ -1,8 +1,9 @@
 require 'discordrb/events/generic'
 require 'discordrb/data'
+require 'discordrb/games'
 
 module Discordrb::Events
-  # Event raised when a user's presence state updates (playing game, idle or back online)
+  # Event raised when a user's presence state updates (idle or online)
   class PresenceEvent
     attr_reader :server, :user, :status
 
@@ -32,6 +33,44 @@ module Discordrb::Events
         matches_all(@attributes[:status], event.status) do |a, e|
           if a.is_a? String
             a == e.to_s
+          else
+            a == e
+          end
+        end
+      ].reduce(true, &:&)
+    end
+  end
+
+  # Event raised when a user plays a game.
+  class PlayingEvent
+    attr_reader :server, :user, :game
+
+    def initialize(data, bot)
+      @user = bot.user(data['user']['id'].to_i)
+      @game = Discordrb::Games.find_game(data['game']['name'])
+      @server = bot.server(data['guild_id'].to_i)
+    end
+  end
+
+  # Event handler for PlayingEvent
+  class PlayingEventHandler < EventHandler
+    def matches?(event)
+      # Check for the proper event type
+      return false unless event.is_a? PlayingEvent
+
+      [
+        matches_all(@attributes[:from], event.user) do |a, e|
+          if a.is_a? String
+            a == e.name
+          elsif a.is_a? Fixnum
+            a == e.id
+          else
+            a == e
+          end
+        end,
+        matches_all(@attributes[:game], event.game) do |a, e|
+          if a.is_a? String
+            a == e.name
           else
             a == e
           end
