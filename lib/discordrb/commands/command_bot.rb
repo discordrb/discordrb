d29bc249a3976474a17d40a5d460819ae83f7f87
meew0 <blactbt@live.de>
2016-01-16 15:25:11 +0100

    Add command aliases to CommandBot

--- lib/discordrb/commands/command_bot.rb
+++ lib/discordrb/commands/command_bot.rb
@@ -82,7 +82,13 @@ module Discordrb::Commands
     end
 
     def command(name, attributes = {}, &block)
-      @commands[name] = Command.new(name, attributes, &block)
+      if name.is_a? Array
+        new_command = Command.new(name[0], attributes, &block)
+        name.each { |n| @commands[n] = new_command }
+        new_command
+      else
+        @commands[name] = Command.new(name, attributes, &block)
+      end
     end
 
     def execute_command(name, event, arguments, chained = false)
