bfa60f585b9ff7a8ae3262e2c1d07f8a51e5b23f
meew0 <blactbt@live.de>
2016-01-23 19:51:44 +0100

    Fix types in VoiceStateUpdateEvent attributes

--- lib/discordrb/events/voice_state_update.rb
+++ lib/discordrb/events/voice_state_update.rb
@@ -32,35 +32,35 @@ module Discordrb::Events
         matches_all(@attributes[:from], event.user) do |a, e|
           a == if a.is_a? String
                  e.name
-               elsif a.is_a? Fixnum
+               elsif a.is_a? Integer
                  e.id
                else
                  e
                end
         end,
         matches_all(@attributes[:mute], event.mute) do |a, e|
-          a == if a.is_a? Boolean
+          a == if a.is_a?(TrueClass) || a.is_a?(FalseClass)
                  e.to_s
                else
                  e
                end
         end,
         matches_all(@attributes[:deaf], event.deaf) do |a, e|
-          a == if a.is_a? Boolean
+          a == if a.is_a?(TrueClass) || a.is_a?(FalseClass)
                  e.to_s
                else
                  e
                end
         end,
         matches_all(@attributes[:self_mute], event.self_mute) do |a, e|
-          a == if a.is_a? Boolean
+          a == if a.is_a?(TrueClass) || a.is_a?(FalseClass)
                  e.to_s
                else
                  e
                end
         end,
         matches_all(@attributes[:self_deaf], event.self_deaf) do |a, e|
-          a == if a.is_a? Boolean
+          a == if a.is_a?(TrueClass) || a.is_a?(FalseClass)
                  e.to_s
                else
                  e
