54467f52c6b7c610ee8da183e0c8f85297b7ee5e
meew0 <blactbt@live.de>
2016-02-01 16:57:58 +0100

    Make a generic IDEvent that only contains an ID

---
+++ lib/discordrb/events/id.rb
@@ -0,0 +1,24 @@
+module Discordrb::Events
+  # An event only associated with an ID and no further information.
+  class IDEvent
+    # @return [Integer] the ID associated with this event
+    attr_reader :id
+
+    # @!visibility private
+    def initialize(id)
+      @id = id
+    end
+  end
+
+  # Event handler for {IDEvent}
+  class IDEventHandler
+    def matches?(event)
+      # Check for the proper event type
+      return false unless event.is_a? IDEvent
+
+      matches_all(@attributes[:id], event.id) do |a, e|
+        a.resolve_id == e.resolve_id
+      end
+    end
+  end
+end
