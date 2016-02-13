f8190d3ed7f2ee027cdb6281b48af94facfcfdfd
meew0 <blactbt@live.de>
2016-02-13 15:04:52 +0100

    Make a special include! method for CommandContainer that also allows including EventContainers

--- lib/discordrb/commands/container.rb
+++ lib/discordrb/commands/container.rb
@@ -1,3 +1,5 @@
+require 'discordrb/containers'
+
 module Discordrb::Commands
   # This module holds a collection of commands that can be easily added to by calling the {CommandContainer#command}
   # function. Other containers can be included into it as well. This allows for modularization of command bots.
@@ -20,10 +22,25 @@ module Discordrb::Commands
 
     # Adds all commands from another container into this one. Existing commands will be overwritten.
     # @param container [Module] A module that `extend`s {CommandContainer} from which the commands will be added.
-    def include!(container)
+    def include_commands(container)
       handlers = container.instance_variable_get '@commands'
       @commands ||= {}
       @commands.merge! handlers
+    end
+
+    def include!(container)
+      container_modules = container.singleton_class.included_modules
+
+      # If the container is an EventContainer and we can include it, then do that
+      if container_modules.include?(Discordrb::EventContainer) && respond_to?(:include_events)
+        include_events(container)
+      end
+
+      if container_modules.include? Discordrb::Commands::CommandContainer
+        include_commands(container)
+      elsif !container_modules.include? Discordrb::EventContainer
+        fail "Could not include! this particular container - ancestors: #{container_modules}"
+      end
     end
   end
 end
