42c8aea1db660c7da4836e1318df1a0a826c2a02
meew0 <blactbt@live.de>
2016-02-14 20:39:57 +0100

    Document CommandContainer#command

--- lib/discordrb/commands/container.rb
+++ lib/discordrb/commands/container.rb
@@ -7,6 +7,30 @@ module Discordrb::Commands
   module CommandContainer
     include RateLimiter
 
+    # Adds a new command to the container.
+    # @param name [Symbol] The name of the command to add.
+    # @param attributes [Hash] The attributes to initialize the command with.
+    # @option attributes [Integer] :permission_level The minimum permission level that can use this command, inclusive.
+    #   See {CommandBot#set_user_permission} and {CommandBot#set_role_permission}.
+    # @option attributes [true, false] :chain_usable Whether this command is able to be used inside of a command chain
+    #   or sub-chain. Typically used for administrative commands that shouldn't be done carelessly.
+    # @option attributes [true, false] :help_available Whether this command is visible in the help command. See the
+    #   :help_command attribute of {CommandBot#initialize}.
+    # @option attributes [String] :description A short description of what this command does. Will be shown in the help
+    #   command if the user asks for it.
+    # @option attributes [String] :usage A short description of how this command should be used. Will be displayed in
+    #   the help command or if the user uses it wrong.
+    # @option attributes [Integer] :min_args The minimum number of arguments this command should have. If a user
+    #   attempts to call the command with fewer arguments, the usage information will be displayed, if it exists.
+    # @option attributes [Integer] :max_args The maximum number of arguments the command should have.
+    # @option attributes [String] :rate_limit_message The message that should be displayed if the command hits a rate
+    #   limit. None if unspecified or nil. %time% in the message will be replaced with the time in seconds when the
+    #   command will be available again.
+    # @option attributes [Symbol] :bucket The rate limit bucket that should be used for rate limiting. No rate limiting
+    #   will be done if unspecified or nil.
+    # @yield The block is executed when the command is executed.
+    # @yieldparam event [CommandEvent] The event of the message that contained the command.
+    # @return [Command] The command that was added.
     def command(name, attributes = {}, &block)
       @commands ||= {}
       if name.is_a? Array
