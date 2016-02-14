7ae73e568bacb739e0c4612c918991f972043ee1
meew0 <blactbt@live.de>
2016-02-14 10:26:33 +0100

    Create the bucket class to represent a rate limit bucket

--- lib/discordrb/commands/rate_limiter.rb
+++ lib/discordrb/commands/rate_limiter.rb
@@ -1,4 +1,23 @@
 module Discordrb::Commands
+  class Bucket
+    # Makes a new bucket
+    # @param limit [Integer, nil] How many requests the user may perform in the given time_span, or nil if there should be no limit.
+    # @param time_span [Integer, nil] The time span after which the request count is reset, in seconds, or nil if the bucket should never be reset. (If this is nil, limit should be nil too)
+    # @param delay [Integer, nil] The delay for which the user has to wait after performing a request, in seconds, or nil if the user shouldn't have to wait.
+    def initialize(limit, time_span, delay)
+      fail ArgumentError, '`limit` and `time_span` have to either both be set or both be nil!' if !limit != !time_span
+
+      @limit = limit
+      @time_span = time_span
+      @delay = delay
+
+      @bucket = {}
+    end
+  end
+
   class RateLimiter
+    def initialize
+
+    end
   end
 end
