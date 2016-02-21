8401f8df121e69b2dc84ca5d53dd406c21673282
meew0 <blactbt@live.de>
2016-02-21 12:42:07 +0100

    Document lifetime events

--- lib/discordrb/events/lifetime.rb
+++ lib/discordrb/events/lifetime.rb
@@ -1,9 +1,15 @@
 require 'discordrb/events/generic'
 
 module Discordrb::Events
+  # @see Discordrb::EventContainer#ready
   class ReadyEvent; end
+
+  # Event handler for {ReadyEvent}
   class ReadyEventHandler < TrueEventHandler; end
 
+  # @see Discordrb::EventContainer#disconnected
   class DisconnectEvent; end
+
+  # Event handler for {DisconnectEvent}
   class DisconnectEventHandler < TrueEventHandler; end
 end
