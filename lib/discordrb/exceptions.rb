f126b19a6aa1d3cf8162fe5f2fd482c9262abfaa
meew0 <blactbt@live.de>
2016-02-21 14:16:04 +0100

    Make a new Errors module for the errors

--- lib/discordrb/exceptions.rb
+++ lib/discordrb/exceptions.rb
@@ -1,18 +1,20 @@
 module Discordrb
-  # Raised when authentication data is invalid or incorrect.
-  class InvalidAuthenticationException < RuntimeError; end
+  module Errors
+    # Raised when authentication data is invalid or incorrect.
+    class InvalidAuthenticationException < RuntimeError; end
 
-  # Raised when a HTTP status code indicates a failure
-  class HTTPStatusException < RuntimeError
-    attr_reader :status
-    def initialize(status)
-      @status = status
+    # Raised when a HTTP status code indicates a failure
+    class HTTPStatusException < RuntimeError
+      attr_reader :status
+      def initialize(status)
+        @status = status
+      end
     end
+
+    # Raised when a message is over the character limit
+    class MessageTooLong < RuntimeError; end
+
+    # Raised when the bot can't do something because its permissions on the server are insufficient
+    class NoPermission < RuntimeError; end
   end
-
-  # Raised when a message is over the character limit
-  class MessageTooLong < RuntimeError; end
-
-  # Raised when the bot can't do something because its permissions on the server are insufficient
-  class NoPermission < RuntimeError; end
 end
