29f8f603934ebc6dacb4f7e3c961bd46cc7e0fcc
meew0 <blactbt@live.de>
2016-02-21 12:41:33 +0100

    Document GuildEvent#init_server

--- lib/discordrb/events/guilds.rb
+++ lib/discordrb/events/guilds.rb
@@ -10,6 +10,8 @@ module Discordrb::Events
       init_server(data, bot)
     end
 
+    # Initializes this event with server data. Should be overwritten in case the server doesn't exist at the time
+    # of event creation (e. g. {GuildDeleteEvent})
     def init_server(data, bot)
       @server = bot.server(data['id'].to_i)
     end
