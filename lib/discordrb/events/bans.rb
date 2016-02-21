71590127ace22fb554afee8c19c2ec488e75d570
meew0 <blactbt@live.de>
2016-02-21 12:25:15 +0100

    Document UserUnbanEventHandler

--- lib/discordrb/events/bans.rb
+++ lib/discordrb/events/bans.rb
@@ -50,5 +50,7 @@ module Discordrb::Events
 
   # Raised when a user is unbanned from a server
   class UserUnbanEvent < UserBanEvent; end
+
+  # Event handler for {UserUnbanEvent}
   class UserUnbanEventHandler < UserBanEventHandler; end
 end
