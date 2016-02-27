97ff30b89e0d408fe469abdba39f94726a128cfc
meew0 <blactbt@live.de>
2016-02-27 13:08:38 +0100

    Add the endpoint for updating an OAuth application

--- lib/discordrb/api.rb
+++ lib/discordrb/api.rb
@@ -139,6 +139,17 @@ module Discordrb::API
     )
   end
 
+  # Change an OAuth application's properties
+  def update_oauth_application(token, name, redirect_uris, description = '', icon = nil)
+    request(
+      :put,
+      "#{APIBASE}/oauth2/applications",
+      { name: name, redirect_uris: redirect_uris, description: description, icon: icon }.to_json,
+      Authorization: token,
+      content_type: :json
+    )
+  end
+
   # Create a server
   def create_server(token, name, region = :london)
     request(
