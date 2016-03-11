6680a7771124446592d0bac6240a918a681f6290
meew0 <blactbt@live.de>
2016-03-11 23:38:39 +0100

    Implement WS connecting

--- lib/discordrb/websocket.rb
+++ lib/discordrb/websocket.rb
@@ -1,5 +1,11 @@
-module Discordrb
-  class WebSocket
+require 'websocket-client-simple'
 
+module Discordrb
+  # Utility wrapper class that abstracts an instance of WSCS. Useful should we decide that WSCS isn't good either -
+  # in that case we can just switch to something else
+  class WebSocket
+    def initialize(endpoint)
+      @client = WebSocket::Client::Simple.connect(endpoint)
+    end
   end
 end
