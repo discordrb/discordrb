be1e0fb3873c4296dcb4ad23e173aa660714213c
meew0 <blactbt@live.de>
2016-03-11 23:52:54 +0100

    Implement WebSocket handlers

--- lib/discordrb/websocket.rb
+++ lib/discordrb/websocket.rb
@@ -4,8 +4,22 @@ module Discordrb
   # Utility wrapper class that abstracts an instance of WSCS. Useful should we decide that WSCS isn't good either -
   # in that case we can just switch to something else
   class WebSocket
-    def initialize(endpoint)
-      @client = WebSocket::Client::Simple.connect(endpoint)
+    attr_reader :open_handler, :message_handler, :close_handler, :error_handler
+
+    def initialize(endpoint, open_handler, message_handler, close_handler, error_handler)
+      @open_handler = open_handler
+      @message_handler = message_handler
+      @close_handler = close_handler
+      @error_handler = error_handler
+
+      instance = self # to work around WSCS's weird way of handling blocks
+
+      @client = WebSocket::Client::Simple.connect(endpoint) do |ws|
+        ws.on(:open) { instance.open_handler.call }
+        ws.on(:message) { |msg| instance.message_handler.call(msg) }
+        ws.on(:close) { |err| instance.close_handler.call(err) }
+        ws.on(:error) { |err| instance.error_handler.call(err) }
+      end
     end
   end
 end
