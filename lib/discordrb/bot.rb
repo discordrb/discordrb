f5cdc3da10df34a69876776456ae9aa55220f938
meew0 <blactbt@live.de>
2016-03-12 15:30:01 +0100

    Move the #channel method from Bot to Cache

--- lib/discordrb/bot.rb
+++ lib/discordrb/bot.rb
@@ -167,29 +167,6 @@ module Discordrb
       @ws_thread.kill
     end
 
-    # Gets a channel given its ID. This queries the internal channel cache, and if the channel doesn't
-    # exist in there, it will get the data from Discord.
-    # @param id [Integer] The channel ID for which to search for.
-    # @return [Channel] The channel identified by the ID.
-    def channel(id)
-      id = id.resolve_id
-
-      raise Discordrb::Errors::NoPermission if @restricted_channels.include? id
-
-      debug("Obtaining data for channel with id #{id}")
-      return @channels[id] if @channels[id]
-
-      begin
-        response = API.channel(token, id)
-        channel = Channel.new(JSON.parse(response), self)
-        @channels[id] = channel
-      rescue Discordrb::Errors::NoPermission
-        debug "Tried to get access to restricted channel #{id}, blacklisting it"
-        @restricted_channels << id
-        raise
-      end
-    end
-
     # Creates a private channel for the given user ID, or if one exists already, returns that one.
     # It is recommended that you use {User#pm} instead, as this is mainly for internal use. However,
     # usage of this method may be unavoidable if only the user ID is known.
--- lib/discordrb/cache.rb
+++ lib/discordrb/cache.rb
@@ -11,5 +11,28 @@ module Discordrb
 
       @restricted_channels = []
     end
+
+    # Gets a channel given its ID. This queries the internal channel cache, and if the channel doesn't
+    # exist in there, it will get the data from Discord.
+    # @param id [Integer] The channel ID for which to search for.
+    # @return [Channel] The channel identified by the ID.
+    def channel(id)
+      id = id.resolve_id
+
+      raise Discordrb::Errors::NoPermission if @restricted_channels.include? id
+
+      debug("Obtaining data for channel with id #{id}")
+      return @channels[id] if @channels[id]
+
+      begin
+        response = API.channel(token, id)
+        channel = Channel.new(JSON.parse(response), self)
+        @channels[id] = channel
+      rescue Discordrb::Errors::NoPermission
+        debug "Tried to get access to restricted channel #{id}, blacklisting it"
+        @restricted_channels << id
+        raise
+      end
+    end
   end
 end
