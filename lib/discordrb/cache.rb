ec5eff07c9c4db799b1726bd1421a60153a3615b
meew0 <blactbt@live.de>
2016-03-12 15:29:39 +0100

    Make a method to initialize the cache hashes

--- lib/discordrb/cache.rb
+++ lib/discordrb/cache.rb
@@ -1,5 +1,15 @@
 module Discordrb
   module Cache
+    # Initializes this cache
+    def init_cache
+      @users = {}
 
+      @servers = {}
+
+      @channels = {}
+      @private_channels = {}
+
+      @restricted_channels = []
+    end
   end
 end
