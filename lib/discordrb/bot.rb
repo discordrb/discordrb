7639a5aed10291398685ce01f26f961d6559411a
meew0 <blactbt@live.de>
2016-03-18 17:59:33 +0100

    Fix a typo in a method call

--- lib/discordrb/bot.rb
+++ lib/discordrb/bot.rb
@@ -665,7 +665,7 @@ module Discordrb
 
     def process_token(type, token)
       # Remove the "Bot " prefix if it exists
-      token = token[4..-1] if token.starts_with? 'Bot '
+      token = token[4..-1] if token.start_with? 'Bot '
 
       token = 'Bot ' + token unless type == :user
       token
