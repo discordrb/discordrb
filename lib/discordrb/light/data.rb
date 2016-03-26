4f41ae217534c2b92bab0e2cf59784e7611b6a9c
meew0 <blactbt@live.de>
2016-03-26 22:22:16 +0100

    Make LightProfile include UserAttributes so it can be used similarly to User

--- lib/discordrb/light/data.rb
+++ lib/discordrb/light/data.rb
@@ -1,6 +1,10 @@
+require 'discordrb/data'
+
 module Discordrb::Light
   # Represents the bot account used for the light bot, but without any methods to change anything.
   class LightProfile
+    include Discordrb::UserAttributes
+
     # @!visibility private
     def initialize(data, bot)
       @bot = bot
