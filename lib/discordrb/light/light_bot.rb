c00e39d432453202aab45a0d44ab52d8018c3741
meew0 <blactbt@live.de>
2016-03-26 22:11:22 +0100

    Write an initializer for LightBot

--- lib/discordrb/light/light_bot.rb
+++ lib/discordrb/light/light_bot.rb
@@ -6,5 +6,11 @@ module Discordrb::Light
   # A bot that only uses the REST part of the API. Hierarchically unrelated to the regular {Discordrb::Bot}. Useful to
   # make applications integrated to Discord over OAuth, for example.
   class LightBot
+    # Create a new LightBot. This does no networking yet, all networking is done by the methods on this class.
+    # @param token [String] The token that should be used to authenticate to Discord. Can be an OAuth token or a regular
+    #   user account token.
+    def initialize(token)
+      @token = token
+    end
   end
 end
