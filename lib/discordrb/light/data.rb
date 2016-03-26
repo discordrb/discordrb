8817e003f57118772cb83fadfb73b7ce1c701bd3
meew0 <blactbt@live.de>
2016-03-26 22:21:50 +0100

    Create an initializer for LightProfile

--- lib/discordrb/light/data.rb
+++ lib/discordrb/light/data.rb
@@ -1,5 +1,21 @@
 module Discordrb::Light
   # Represents the bot account used for the light bot, but without any methods to change anything.
   class LightProfile
+    # @!visibility private
+    def initialize(data, bot)
+      @bot = bot
+
+      @username = data['username']
+      @id = data['id'].to_i
+      @discriminator = data['discriminator']
+      @avatar_id = data['avatar']
+
+      @bot_account = false
+      @bot_account = true if data['bot']
+
+      @verified = data['verified']
+
+      @email = data['email']
+    end
   end
 end
