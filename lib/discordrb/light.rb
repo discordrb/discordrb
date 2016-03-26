839b16f43714b6f40003daf7777c5df32cdbfd01
meew0 <blactbt@live.de>
2016-03-26 23:07:42 +0100

    Add a file to conveniently include light bot stuff

---
+++ lib/discordrb/light.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'discordrb/light/light_bot'
+
+# This module contains classes to allow connections to bots without a connection to the gateway socket, i. e. bots
+# that only use the REST part of the API.
+module Discordrb::Light
+end
