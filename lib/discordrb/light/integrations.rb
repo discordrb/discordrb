8152f8956ab00d3840ae00f4765f995e70061b63
meew0 <blactbt@live.de>
2016-03-28 00:45:01 +0200

    Add some attributes to Connection

--- lib/discordrb/light/integrations.rb
+++ lib/discordrb/light/integrations.rb
@@ -1,5 +1,17 @@
 module Discordrb::Light
   # A connection of your Discord account to a particular other service (currently, Twitch and YouTube)
   class Connection
+    # @return [Symbol] what type of connection this is (either :twitch or :youtube currently)
+    attr_reader :type
+
+    # @return [true, false] whether this connection is revoked
+    attr_reader :revoked
+    alias_method :revoked?, :revoked
+
+    # @return [String] the name of the connected account
+    attr_reader :name
+
+    # @return [String] the ID of the connected account
+    attr_reader :id
   end
 end
