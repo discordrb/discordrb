b5ed1296a4b5e3f4843f4f83f698fc07315b7b08
meew0 <blactbt@live.de>
2016-03-29 16:39:38 +0200

    Add a method to split messages into chunks of 2000 (at most) characters, fixes #74

--- lib/discordrb/data.rb
+++ lib/discordrb/data.rb
@@ -19,6 +19,48 @@ module Discordrb
     other.respond_to?(:resolve_id) ? (one_id.resolve_id == other.resolve_id) : (one_id == other)
   end
 
+  # The maximum length a Discord message can have
+  CHARACTER_LIMIT = 2000
+
+  # Splits a message into chunks of 2000 characters. Attempts to split by lines if possible.
+  # @param msg [String] The message to split.
+  # @return [Array<String>] the message split into chunks
+  def self.split_message(msg)
+    # If the messages is empty, return an empty array
+    return [] if msg.empty?
+
+    # Split the message into lines
+    lines = msg.lines
+
+    # Turn the message into a "triangle" of consecutively longer slices, for example the array [1,2,3,4] would become
+    # [
+    #  [1],
+    #  [1, 2],
+    #  [1, 2, 3],
+    #  [1, 2, 3, 4]
+    # ]
+    tri = [*0..(lines.length - 1)].map { |i| lines.combination(i + 1).first }
+
+    # Join the individual elements together to get an array of strings with consecutively more lines
+    joined = tri.map { |e| e.join("\n") }
+
+    # Find the largest element that is still below the character limit, or if none such element exists return the first
+    ideal = joined.max_by { |e| e.length > CHARACTER_LIMIT ? -1 : e.length }
+
+    # If it's still larger than the character limit (none was smaller than it) split it into slices with the length
+    # being the character limit, otherwise just return an array with one element
+    ideal_ary = (ideal.length > CHARACTER_LIMIT) ? ideal.chars.each_slice(CHARACTER_LIMIT).map(&:join) : [ideal]
+
+    # Slice off the ideal part and strip newlines
+    rest = msg[ideal.length..-1].strip
+
+    # If none remains, return an empty array -> we're done
+    return [] unless rest
+
+    # Otherwise, call the method recursively to split the rest of the string and add it onto the ideal array
+    ideal_ary + split_message(rest)
+  end
+
   # Mixin for objects that have IDs
   module IDObject
     # @return [Integer] the ID which uniquely identifies this object across Discord.
