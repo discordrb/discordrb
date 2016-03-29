735cd17cbf753f56a26b054dec89d96eefbe2b58
meew0 <blactbt@live.de>
2016-03-29 22:25:58 +0200

    Raise an error if an unknown request gets RLed

--- lib/discordrb/api.rb
+++ lib/discordrb/api.rb
@@ -72,6 +72,8 @@ module Discordrb::API
 
       response = raw_request(type, attributes)
     rescue RestClient::TooManyRequests
+      raise "Got an HTTP 429 for an untracked API call! Please report this bug together with the following information: #{type} #{attributes}" unless key
+
       wait_seconds = response[:retry_after].to_i / 1000.0
       LOGGER.warn("Locking RL mutex (key: #{key}) for #{wait_seconds} seconds due to Discord rate limiting")
 
