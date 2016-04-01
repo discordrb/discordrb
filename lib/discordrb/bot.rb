c48339e99924237ff6cab3b8a4395d6ab99a0934
meew0 <blactbt@live.de>
2016-04-02 01:02:50 +0200

    Add all the opcode with comprehensive doc comments to the Opcodes module

--- lib/discordrb/bot.rb
+++ lib/discordrb/bot.rb
@@ -28,6 +28,52 @@ require 'discordrb/voice/voice_bot'
 module Discordrb
   # Gateway packet opcodes
   module Opcodes
+    # **Received** when Discord dispatches an event to the gateway (like MESSAGE_CREATE, PRESENCE_UPDATE or whatever).
+    # The vast majority of received packets will have this opcode.
+    DISPATCH = 0
+
+    # **Two-way**: The client has to send a packet with this opcode every ~40 seconds (actual interval specified in
+    # READY or RESUMED) and the current sequence number, otherwise it will be disconnected from the gateway. In certain
+    # cases Discord may also send one, specifically if two clients are connected at once.
+    HEARTBEAT = 1
+
+    # **Sent**: This is one of the two possible ways to initiate a session after connecting to the gateway. It
+    # should contain the authentication token along with other stuff the server has to know right from the start, such
+    # as large_threshold and, for older gateway versions, the desired version.
+    IDENTIFY = 2
+
+    # **Sent**: Packets with this opcode are used to change the user's status and played game. (Sending this is never
+    # necessary for a gateway client to behave correctly)
+    PRESENCE = 3
+
+    # **Sent**: Packets with this opcode are used to change a user's voice state (mute/deaf/unmute/undeaf/etc.).
+    # (Sending this is never necessary for a gateway client to behave correctly)
+    VOICE_STATE = 4
+
+    # **Sent**: This opcode is used to connect to a voice server. (Sending this is never necessary for a gateway client
+    # to behave correctly)
+    VOICE_SERVER = 5
+
+    # **Sent**: This is the other of two possible ways to initiate a gateway session (other than {IDENTIFY}). Rather
+    # than starting an entirely new session, it resumes an existing session by replaying all events from a given
+    # sequence number. It should be used to recover from a connection error or anything like that when the session is
+    # still valid - sending this with an invalid session will cause an error to occur.
+    RESUME = 6
+
+    # **Received**: Discord sends this opcode to indicate that the client should reconnect to a different gateway
+    # server because the old one is currently being decommissioned. Counterintuitively, this opcode also invalidates the
+    # session - the client has to create an entirely new session with the new gateway instead of resuming the old one.
+    RECONNECT = 7
+
+    # **Sent**: This opcode identifies packets used to retrieve a list of members from a particular server. There is
+    # also a REST endpoint available for this, but it is inconvenient to use because the client has to implement
+    # pagination itself, whereas sending this opcode lets Discord handle the pagination and the client can just add
+    # members when it receives them. (Sending this is never necessary for a gateway client to behave correctly)
+    REQUEST_MEMBERS = 8
+
+    # **Received**: The functionality of this opcode is less known than the others but it appears to specifically
+    # tell the client to invalidate its local session and continue by {IDENTIFY}ing.
+    INVALIDATE_SESSION = 9
   end
 
   # Represents a Discord bot, including servers, users, etc.
