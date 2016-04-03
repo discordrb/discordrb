68b5dade14fd7335949a40490b5ada6695dae5fa
meew0 <blactbt@live.de>
2016-04-03 16:04:28 +0200

    Make inject_resume take a seq argument

--- lib/discordrb/bot.rb
+++ lib/discordrb/bot.rb
@@ -494,8 +494,9 @@ module Discordrb
     # Injects a resume packet (op 6) into the gateway. If this is done with a running connection, it will cause an
     # error. It has no use outside of testing stuff that I know of, but if you want to use it anyway for some reason,
     # here it is.
-    def inject_resume
-      resume(@sequence, raw_token, @session_id)
+    # @param seq [Integer, nil] The sequence ID to inject, or nil if the currently tracked one should be used.
+    def inject_resume(seq)
+      resume(seq || @sequence, raw_token, @session_id)
     end
 
     # Sets debug mode. If debug mode is on, many things will be outputted to STDOUT.
