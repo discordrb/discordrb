950d0f0a6ce2ee537f65587575916ca465783132
meew0 <blactbt@live.de>
2016-04-07 13:06:45 +0200

    Initial connection logic

--- spec/ldash_helper.rb
+++ spec/ldash_helper.rb
@@ -1,6 +1,22 @@
+require 'rest-client'
+require 'json'
+
 module LdashClient
+  LDASH_URL = 'http://127.0.0.1:6601'.freeze
+
   class Session
-    def initialize
+    def initialize(preset)
+      connect(preset)
+    end
+
+    private
+
+    def connect(preset)
+      RestClient.post(
+        LDASH_URL + '/session',
+        { preset: preset }.to_json,
+        content_type: json
+      )
     end
   end
 end
