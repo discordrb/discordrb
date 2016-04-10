ba995be0b8dfd6f6818d9f5ca4f3a357f65d78fa
meew0 <blactbt@live.de>
2016-04-10 11:50:20 +0200

    Rewrite DISCORDRB_NONACL handling to avoid the warning about constant reassignment

--- lib/discordrb/voice/network.rb
+++ lib/discordrb/voice/network.rb
@@ -8,14 +8,16 @@ require 'json'
 require 'discordrb/websocket'
 
 begin
-  RBNACL_AVAILABLE = false
-  unless ENV.key?('DISCORDRB_NONACL')
-    require 'rbnacl'
-    RBNACL_AVAILABLE = true
-  end
+  RBNACL_AVAILABLE = if ENV['DISCORDRB_NONACL']
+                       false
+                     else
+                       require 'rbnacl'
+                       true
+                     end
 rescue LoadError
   puts "libsodium not available! You can continue to use discordrb as normal but voice support won't work.
         Read https://github.com/meew0/discordrb/wiki/Installing-libsodium for more details."
+  RBNACL_AVAILABLE = false
 end
 
 module Discordrb::Voice
