b434656fab8d04f94891c2a1c88b62ea8440611e
snapcase <snapcase@octalzeroes.com>
2016-04-15 18:03:18 +0200

    new authentication in examples

--- examples/ping_with_respond_time.rb
+++ examples/ping_with_respond_time.rb
@@ -2,7 +2,7 @@
 
 require 'discordrb'
 
-bot = Discordrb::Commands::CommandBot.new email: 'email@example.com', password: 'hunter2', prefix: '!'
+bot = Discordrb::Commands::CommandBot.new token: 'B0T.T0KEN.here', application_id: 160123456789876543, prefix: '!'
 
 bot.command(:ping) do |event|
   m = event.respond('Pong!')
