10365fad904ec70c428bdb10b9e4f0f9ed47babf
meew0 <blactbt@live.de>
2016-04-17 12:05:31 +0200

    Output the bot's invite URL in the ping example

--- examples/ping.rb
+++ examples/ping.rb
@@ -4,6 +4,11 @@ require 'discordrb'
 
 bot = Discordrb::Bot.new token: 'B0T.T0KEN.here', application_id: 160123456789876543
 
+# Here we output the invite URL to the console so the bot account can be invited to the channel. This only has to be
+# done once, afterwards, you can remove this part if you want
+puts "This bot's invite URL is #{bot.invite_url}."
+puts 'Click on it to invite it to your server.'
+
 bot.message(with_text: 'Ping!') do |event|
   event.respond 'Pong!'
 end
