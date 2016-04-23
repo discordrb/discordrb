e65aa7a8e08b13c41c32983d4ab02da8265d9d92
meew0 <blactbt@live.de>
2016-04-23 13:49:24 +0200

    Make a case block to check for different prefix types

--- lib/discordrb/commands/command_bot.rb
+++ lib/discordrb/commands/command_bot.rb
@@ -224,8 +224,11 @@ module Discordrb::Commands
 
     # Check whether a message should trigger command execution, and if it does, return the raw chain
     def trigger?(message)
-      return nil unless message.content.start_with? @prefix
-      message.content[@prefix.length..-1]
+      case @prefix
+      when String
+        return nil unless message.content.start_with? @prefix
+        message.content[@prefix.length..-1]
+      end
     end
 
     def execute_chain(chain, event)
