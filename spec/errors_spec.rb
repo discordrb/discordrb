581cf52b2072e162fe28c125548878549d1c85d2
meew0 <blactbt@live.de>
2016-05-29 23:14:44 +0200

    Use expect syntax because should syntax breaks for some reason

--- spec/errors_spec.rb
+++ spec/errors_spec.rb
@@ -9,14 +9,14 @@ describe Discordrb::Errors do
     describe 'the created class' do
       it 'should contain the correct code' do
         classy = Discordrb::Errors.Code(10_001)
-        classy.code should eq(10_001)
+        expect(classy.code).to eq(10_001)
       end
 
       it 'should create an instance with the correct code' do
         classy = Discordrb::Errors.Code(10_002)
         error = classy.new 'random message'
-        error.code should eq(10_002)
-        error.message should eq 'random message'
+        expect(error.code).to eq(10_002)
+        expect(error.message).to eq 'random message'
       end
     end
   end
