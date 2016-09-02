0f6fefdfd6c326fdef62d5c2fad312d76819a48d
meew0 <blactbt@live.de>
2016-09-02 19:27:49 +0200

    Stop the voice connection in voice_send.rb unless the user is in a voice channel

--- examples/voice_send.rb
+++ examples/voice_send.rb
@@ -7,6 +7,10 @@ bot.command(:connect) do |event|
   # voice channel.
   channel = event.user.voice_channel
 
+  # Here we return from the command unless the channel is not nil (i. e. the user is in a voice channel). The `next`
+  # construct can be used to exit a command prematurely, and even send a message while we're at it.
+  next "You're not in any voice channel!" unless channel
+
   bot.voice_connect(channel)
   "Connected to voice channel: #{channel.name}"
 end
