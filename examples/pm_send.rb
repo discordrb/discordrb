086a91bf5dcc379b1fc3c7e4b030608547b3b6ee
meew0 <blactbt@live.de>
2016-09-02 19:02:07 +0200

    Clarify what the `mention` event does in pm_send.rb

--- examples/pm_send.rb
+++ examples/pm_send.rb
@@ -4,6 +4,7 @@ require 'discordrb'
 
 bot = Discordrb::Bot.new token: 'B0T.T0KEN.here', application_id: 160123456789876543
 
+# The `mention` event is called if the bot is *directly mentioned*, i.e. not using a role mention or @everyone/@here.
 bot.mention do |event|
   event.user.pm('You have mentioned me!')
 end
