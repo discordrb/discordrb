32d3d147ade5209b8f0c17c299fe1d5009e44a46
meew0 <blactbt@live.de>
2016-09-02 11:03:00 +0200

    Document the `bot.run` at the end of the ping example

--- examples/ping.rb
+++ examples/ping.rb
@@ -24,4 +24,6 @@ bot.message(content: 'Ping!') do |event|
   event.respond 'Pong!'
 end
 
+# This method call has to be put at the end of your script, it is what makes the bot actually connect to Discord. If you
+# leave it out (try it!) the script will simply stop and the bot will not appear online.
 bot.run
