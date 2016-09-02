b42bc843b1e0755fd0c873dbd44b5b7fdb437c2d
meew0 <blactbt@live.de>
2016-09-02 19:00:55 +0200

    Add a comment to ping_with_respond_time

--- examples/ping_with_respond_time.rb
+++ examples/ping_with_respond_time.rb
@@ -6,6 +6,8 @@ require 'discordrb'
 bot = Discordrb::Bot.new token: 'B0T.T0KEN.here', application_id: 160123456789876543
 
 bot.message(content: 'Ping!') do |event|
+  # The `respond` method returns a `Message` object, which is stored in a variable `m`. The `edit` method is then called
+  # to edit the message with the time difference between when the event was received and after the message was sent.
   m = event.respond('Pong!')
   m.edit "Pong! Time taken: #{Time.now - event.timestamp} seconds."
 end
