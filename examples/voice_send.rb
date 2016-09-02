e52f3d0ee736119bc6f0eef410ad374277ebdad4
meew0 <blactbt@live.de>
2016-09-02 19:26:45 +0200

    Document the call to `voice_channel`

--- examples/voice_send.rb
+++ examples/voice_send.rb
@@ -3,6 +3,8 @@ require 'discordrb'
 bot = Discordrb::Commands::CommandBot.new token: 'B0T.T0KEN.here', application_id: 160123456789876543, prefix: '!'
 
 bot.command(:connect) do |event|
+  # The `voice_channel` method returns the voice channel the user is currently in, or `nil` if the user is not in a
+  # voice channel.
   channel = event.user.voice_channel
 
   bot.voice_connect(channel)
