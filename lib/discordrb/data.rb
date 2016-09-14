d45ec7869d8167dfb33d5a162fa0ecc6023c220d
Jonathan B <greenbigfrog@gmail.com>
2016-09-14 21:20:23 +0200

    add emoji stuff to Message

--- lib/discordrb/data.rb
+++ lib/discordrb/data.rb
@@ -1445,6 +1445,9 @@ module Discordrb
     # @return [Array<User>] the users that were mentioned in this message.
     attr_reader :mentions
 
+    # @return [Array<Emoji>] the emotes that were used/mentioned in this message (Only returns Emoji the bot has access to, else nil).
+    attr_reader :emoji
+
     # @return [Array<Role>] the roles that were mentioned in this message.
     attr_reader :role_mentions
 
@@ -1500,6 +1503,14 @@ module Discordrb
       @edited_timestamp = data['edited_timestamp'].nil? ? nil : Time.parse(data['edited_timestamp'])
       @edited = !@edited_timestamp.nil?
       @id = data['id'].to_i
+
+      @emoji = []
+
+      emoji = data['content'].split
+      emoji = emoji.grep(/<:(?<name>\w+):(?<id>\d+)>?/)
+      emoji.each do |element|
+        @emoji << bot.parse_mention(element)
+      end
 
       @mentions = []
 
@@ -1566,6 +1577,12 @@ module Discordrb
     # @return [true, false] whether this message was sent by the current {Bot}.
     def from_bot?
       @author && @author.current_bot?
+    end
+
+    # Check if any emoji got used in this message
+    # @return [boolean] wether any emoji got used or not
+    def emoji?
+      return true unless @emoji.empty?
     end
 
     # The inspect method is overwritten to give more useful output
