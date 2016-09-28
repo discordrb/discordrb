d95191a6dc5655807ff3560ac7292de693bbfd1c
Daniel Worrall <Daniel.Worrall@hotmail.co.uk>
2016-09-28 16:23:56 +0100

    Fixed consistency in Permission comments

--- lib/discordrb/permissions.rb
+++ lib/discordrb/permissions.rb
@@ -36,7 +36,7 @@ module Discordrb
       26 => :change_nickname,      # 67108864
       27 => :manage_nicknames,     # 134217728
       28 => :manage_roles,         # 268435456, also Manage Permissions
-      # 29
+      # 29                         # 536870912
       30 => :manage_emojis         # 1073741824
     }.freeze
 
