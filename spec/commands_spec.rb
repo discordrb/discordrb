bcf6ca6fdaafdd10938bff0d150a5a1892752eff
meew0 <blactbt@live.de>
2016-10-09 14:02:10 +0200

    Make the "no defined commands" a context with two tests

--- spec/commands_spec.rb
+++ spec/commands_spec.rb
@@ -2,15 +2,22 @@ require 'discordrb'
 
 describe Discordrb::Commands do
   describe Discordrb::Commands::CommandBot do
-    it 'should allow no defined commands' do
+    context 'no defined commands' do
       bot = Discordrb::Commands::CommandBot.new token: '', help_available: false
 
-      event = spy
+      it 'should successfully trigger the command' do
+        event = double
 
-      bot.execute_command(:test, event, [])
+        bot.execute_command(:test, event, [])
+      end
 
-      # We don't want anything sent to the channel
-      expect(spy).to_not have_received :respond
+      it 'should not send anything to the channel' do
+        event = spy
+
+        bot.execute_command(:test, event, [])
+
+        expect(spy).to_not have_received :respond
+      end
     end
   end
 end
