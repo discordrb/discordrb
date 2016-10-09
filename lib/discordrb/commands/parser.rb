869718b514e9c8deaa50416054a2a87f5855808e
meew0 <blactbt@live.de>
2016-10-09 17:02:28 +0200

    Also create check_permission for Command#call to prevent rate limit checking

--- lib/discordrb/commands/parser.rb
+++ lib/discordrb/commands/parser.rb
@@ -61,8 +61,10 @@ module Discordrb::Commands
     # @param event [CommandEvent] The event to call the command with.
     # @param arguments [Array<String>] The attributes for the command.
     # @param chained [true, false] Whether or not this command is part of a command chain.
+    # @param check_permission [true, false] Whether the user's permission to execute the command (i.e. rate limits)
+    #   should be checked.
     # @return [String] the result of the execution.
-    def call(event, arguments, chained = false)
+    def call(event, arguments, chained = false, check_permission = true)
       if arguments.length < @attributes[:min_args]
         event.respond "Too few arguments for command `#{name}`!"
         event.respond "Usage: `#{@attributes[:usage]}`" if @attributes[:usage]
@@ -80,12 +82,14 @@ module Discordrb::Commands
         end
       end
 
-      rate_limited = event.bot.rate_limited?(@attributes[:bucket], event.author)
-      if @attributes[:bucket] && rate_limited
-        if @attributes[:rate_limit_message]
-          event.respond @attributes[:rate_limit_message].gsub('%time%', rate_limited.round(2).to_s)
+      if check_permission
+        rate_limited = event.bot.rate_limited?(@attributes[:bucket], event.author)
+        if @attributes[:bucket] && rate_limited
+          if @attributes[:rate_limit_message]
+            event.respond @attributes[:rate_limit_message].gsub('%time%', rate_limited.round(2).to_s)
+          end
+          return
         end
-        return
       end
 
       result = @block.call(event, *arguments)
