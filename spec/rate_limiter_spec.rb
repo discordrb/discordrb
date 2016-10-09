2190e6c2c711dda689cc07ed4f96bf3a87be2758
meew0 <blactbt@live.de>
2016-10-09 13:37:35 +0200

    Use expect syntax exclusively in rate_limiter_spec

--- spec/rate_limiter_spec.rb
+++ spec/rate_limiter_spec.rb
@@ -7,11 +7,11 @@ RATELIMITER = Discordrb::Commands::RateLimiter
 describe Discordrb::Commands::Bucket do
   describe 'rate_limited?' do
     it 'should not rate limit one request' do
-      BUCKET.new(1, 5, 2).rate_limited?(:a).should be_falsy
-      BUCKET.new(nil, nil, 2).rate_limited?(:a).should be_falsy
-      BUCKET.new(1, 5, nil).rate_limited?(:a).should be_falsy
-      BUCKET.new(0, 1, nil).rate_limited?(:a).should be_falsy
-      BUCKET.new(0, 1_000_000_000, 500_000_000).rate_limited?(:a).should be_falsy
+      expect(BUCKET.new(1, 5, 2).rate_limited?(:a)).to be_falsy
+      expect(BUCKET.new(nil, nil, 2).rate_limited?(:a)).to be_falsy
+      expect(BUCKET.new(1, 5, nil).rate_limited?(:a)).to be_falsy
+      expect(BUCKET.new(0, 1, nil).rate_limited?(:a)).to be_falsy
+      expect(BUCKET.new(0, 1_000_000_000, 500_000_000).rate_limited?(:a)).to be_falsy
     end
 
     it 'should fail to initialize with invalid arguments' do
@@ -24,79 +24,79 @@ describe Discordrb::Commands::Bucket do
 
     it 'should rate limit one request over the limit' do
       b = BUCKET.new(1, 5, nil)
-      b.rate_limited?(:a).should be_falsy
-      b.rate_limited?(:a).should be_truthy
+      expect(b.rate_limited?(:a)).to be_falsy
+      expect(b.rate_limited?(:a)).to be_truthy
     end
 
     it 'should rate limit multiple requests that are over the limit' do
       b = BUCKET.new(3, 5, nil)
-      b.rate_limited?(:a).should be_falsy
-      b.rate_limited?(:a).should be_falsy
-      b.rate_limited?(:a).should be_falsy
-      b.rate_limited?(:a).should be_truthy
+      expect(b.rate_limited?(:a)).to be_falsy
+      expect(b.rate_limited?(:a)).to be_falsy
+      expect(b.rate_limited?(:a)).to be_falsy
+      expect(b.rate_limited?(:a)).to be_truthy
     end
 
     it 'should not rate limit after the limit ran out' do
       b = BUCKET.new(2, 5, nil)
-      b.rate_limited?(:a).should be_falsy
-      b.rate_limited?(:a).should be_falsy
-      b.rate_limited?(:a).should be_truthy
-      b.rate_limited?(:a, Time.now + 4).should be_truthy
-      b.rate_limited?(:a, Time.now + 5).should be_falsy
+      expect(b.rate_limited?(:a)).to be_falsy
+      expect(b.rate_limited?(:a)).to be_falsy
+      expect(b.rate_limited?(:a)).to be_truthy
+      expect(b.rate_limited?(:a, Time.now + 4)).to be_truthy
+      expect(b.rate_limited?(:a, Time.now + 5)).to be_falsy
     end
 
     it 'should reset the limit after it ran out' do
       b = BUCKET.new(2, 5, nil)
-      b.rate_limited?(:a).should be_falsy
-      b.rate_limited?(:a).should be_falsy
-      b.rate_limited?(:a).should be_truthy
-      b.rate_limited?(:a, Time.now + 5).should be_falsy
-      b.rate_limited?(:a, Time.now + 5.01).should be_falsy
-      b.rate_limited?(:a, Time.now + 5.02).should be_truthy
+      expect(b.rate_limited?(:a)).to be_falsy
+      expect(b.rate_limited?(:a)).to be_falsy
+      expect(b.rate_limited?(:a)).to be_truthy
+      expect(b.rate_limited?(:a, Time.now + 5)).to be_falsy
+      expect(b.rate_limited?(:a, Time.now + 5.01)).to be_falsy
+      expect(b.rate_limited?(:a, Time.now + 5.02)).to be_truthy
     end
 
     it 'should rate limit based on delay' do
       b = BUCKET.new(nil, nil, 2)
-      b.rate_limited?(:a).should be_falsy
-      b.rate_limited?(:a).should be_truthy
+      expect(b.rate_limited?(:a)).to be_falsy
+      expect(b.rate_limited?(:a)).to be_truthy
     end
 
     it 'should not rate limit after the delay ran out' do
       b = BUCKET.new(nil, nil, 2)
-      b.rate_limited?(:a).should be_falsy
-      b.rate_limited?(:a).should be_truthy
-      b.rate_limited?(:a, Time.now + 2).should be_falsy
-      b.rate_limited?(:a, Time.now + 2).should be_truthy
-      b.rate_limited?(:a, Time.now + 4).should be_falsy
-      b.rate_limited?(:a, Time.now + 4).should be_truthy
+      expect(b.rate_limited?(:a)).to be_falsy
+      expect(b.rate_limited?(:a)).to be_truthy
+      expect(b.rate_limited?(:a, Time.now + 2)).to be_falsy
+      expect(b.rate_limited?(:a, Time.now + 2)).to be_truthy
+      expect(b.rate_limited?(:a, Time.now + 4)).to be_falsy
+      expect(b.rate_limited?(:a, Time.now + 4)).to be_truthy
     end
 
     it 'should rate limit based on both limit and delay' do
       b = BUCKET.new(2, 5, 2)
-      b.rate_limited?(:a).should be_falsy
-      b.rate_limited?(:a).should be_truthy
-      b.rate_limited?(:a, Time.now + 2).should be_falsy
-      b.rate_limited?(:a, Time.now + 2).should be_truthy
-      b.rate_limited?(:a, Time.now + 4).should be_truthy
-      b.rate_limited?(:a, Time.now + 5).should be_falsy
-      b.rate_limited?(:a, Time.now + 6).should be_truthy
+      expect(b.rate_limited?(:a)).to be_falsy
+      expect(b.rate_limited?(:a)).to be_truthy
+      expect(b.rate_limited?(:a, Time.now + 2)).to be_falsy
+      expect(b.rate_limited?(:a, Time.now + 2)).to be_truthy
+      expect(b.rate_limited?(:a, Time.now + 4)).to be_truthy
+      expect(b.rate_limited?(:a, Time.now + 5)).to be_falsy
+      expect(b.rate_limited?(:a, Time.now + 6)).to be_truthy
 
       b = BUCKET.new(2, 5, 2)
-      b.rate_limited?(:a).should be_falsy
-      b.rate_limited?(:a).should be_truthy
-      b.rate_limited?(:a, Time.now + 4).should be_falsy
-      b.rate_limited?(:a, Time.now + 4).should be_truthy
-      b.rate_limited?(:a, Time.now + 5).should be_truthy
+      expect(b.rate_limited?(:a)).to be_falsy
+      expect(b.rate_limited?(:a)).to be_truthy
+      expect(b.rate_limited?(:a, Time.now + 4)).to be_falsy
+      expect(b.rate_limited?(:a, Time.now + 4)).to be_truthy
+      expect(b.rate_limited?(:a, Time.now + 5)).to be_truthy
     end
 
     it 'should return correct times' do
       start_time = Time.now
       b = BUCKET.new(2, 5, 2)
-      b.rate_limited?(:a, start_time).should be_falsy
-      b.rate_limited?(:a, start_time).round(2).should eq(2)
-      b.rate_limited?(:a, start_time + 1).round(2).should eq(1)
-      b.rate_limited?(:a, start_time + 2.01).should be_falsy
-      b.rate_limited?(:a, start_time + 2).round(2).should eq(3)
+      expect(b.rate_limited?(:a, start_time)).to be_falsy
+      expect(b.rate_limited?(:a, start_time).round(2)).to eq(2)
+      expect(b.rate_limited?(:a, start_time + 1).round(2)).to eq(1)
+      expect(b.rate_limited?(:a, start_time + 2.01)).to be_falsy
+      expect(b.rate_limited?(:a, start_time + 2).round(2)).to eq(3)
     end
   end
 end
