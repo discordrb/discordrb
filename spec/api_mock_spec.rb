d876932abe8dbb2733f42ec6ee2022ea486e8773
meew0 <blactbt@live.de>
2016-10-09 13:28:25 +0200

    Write some more specs for APIMock

--- spec/api_mock_spec.rb
+++ spec/api_mock_spec.rb
@@ -8,4 +8,24 @@ describe APIMock do
 
     Discordrb::API.last_method.should == :get
   end
+
+  it 'stores the used URL' do
+    url = 'https://example.com/test'
+    Discordrb::API.raw_request(:get, [url])
+
+    Discordrb::API.last_url.should == url
+  end
+
+  it 'parses the stored body using JSON' do
+    body = { test: 1 }
+    Discordrb::API.raw_request(:post, ['https://example.com/test', body.to_json])
+
+    Discordrb::API.last_body['test'].should == 1
+  end
+
+  it "doesn't parse the body if there is none present" do
+    Discordrb::API.raw_request(:post, ['https://example.com/test', nil])
+
+    Discordrb::API.last_body.should be_nil
+  end
 end
