8ab1968f30ce8a9c7de1012a7b99e5b7d2992432
meew0 <blactbt@live.de>
2016-10-09 13:35:31 +0200

    Use expect syntax exclusively in event_spec

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -8,93 +8,93 @@ describe Discordrb::Events do
 
     it 'should contain the passed object' do
       negated = Discordrb::Events::Negated.new(:test)
-      negated.object.should == :test
+      expect(negated.object).to eq :test
     end
   end
 
   describe 'not!' do
     it 'should return a Negated object' do
-      not!(:test).should be_a(Discordrb::Events::Negated)
+      expect(not!(:test)).to be_a(Discordrb::Events::Negated)
     end
 
     it 'should contain the correct value' do
-      not!(:test).object.should == :test
+      expect(not!(:test).object).to eq :test
     end
   end
 
   describe 'matches_all' do
     it 'should return true for a nil attribute' do
-      Discordrb::Events.matches_all(nil, nil).should == true
+      expect(Discordrb::Events.matches_all(nil, nil)).to eq true
     end
 
     it 'should be truthy if the block is truthy' do
-      Discordrb::Events.matches_all(:a, :e) { true }.should be_truthy
-      Discordrb::Events.matches_all(:a, :e) { 1 }.should be_truthy
-      Discordrb::Events.matches_all(:a, :e) { 0 }.should be_truthy
-      Discordrb::Events.matches_all(:a, :e) { 'string' }.should be_truthy
-      Discordrb::Events.matches_all(:a, :e) { false }.should_not be_truthy
+      expect(Discordrb::Events.matches_all(:a, :e) { true }).to be_truthy
+      expect(Discordrb::Events.matches_all(:a, :e) { 1 }).to be_truthy
+      expect(Discordrb::Events.matches_all(:a, :e) { 0 }).to be_truthy
+      expect(Discordrb::Events.matches_all(:a, :e) { 'string' }).to be_truthy
+      expect(Discordrb::Events.matches_all(:a, :e) { false }).to_not be_truthy
     end
 
     it 'should be falsey if the block is falsey' do
-      Discordrb::Events.matches_all(:a, :e) { nil }.should be_falsy
-      Discordrb::Events.matches_all(:a, :e) { false }.should be_falsy
-      Discordrb::Events.matches_all(:a, :e) { 0 }.should_not be_falsy
+      expect(Discordrb::Events.matches_all(:a, :e) { nil }).to be_falsy
+      expect(Discordrb::Events.matches_all(:a, :e) { false }).to be_falsy
+      expect(Discordrb::Events.matches_all(:a, :e) { 0 }).to_not be_falsy
     end
 
     it 'should correctly pass the arguments given' do
       Discordrb::Events.matches_all(:one, :two) do |a, e|
-        a.should eq(:one)
-        e.should eq(:two)
+        expect(a).to eq(:one)
+        expect(e).to eq(:two)
       end
     end
 
     it 'should correctly compare arguments for comparison blocks' do
-      Discordrb::Events.matches_all(1, 1) { |a, e| a == e }.should be_truthy
-      Discordrb::Events.matches_all(1, 0) { |a, e| a == e }.should be_falsy
-      Discordrb::Events.matches_all(0, 1) { |a, e| a == e }.should be_falsy
-      Discordrb::Events.matches_all(0, 0) { |a, e| a == e }.should be_truthy
-      Discordrb::Events.matches_all(1, 1) { |a, e| a != e }.should be_falsy
-      Discordrb::Events.matches_all(1, 0) { |a, e| a != e }.should be_truthy
-      Discordrb::Events.matches_all(0, 1) { |a, e| a != e }.should be_truthy
-      Discordrb::Events.matches_all(0, 0) { |a, e| a != e }.should be_falsy
+      expect(Discordrb::Events.matches_all(1, 1) { |a, e| a == e }).to be_truthy
+      expect(Discordrb::Events.matches_all(1, 0) { |a, e| a == e }).to be_falsy
+      expect(Discordrb::Events.matches_all(0, 1) { |a, e| a == e }).to be_falsy
+      expect(Discordrb::Events.matches_all(0, 0) { |a, e| a == e }).to be_truthy
+      expect(Discordrb::Events.matches_all(1, 1) { |a, e| a != e }).to be_falsy
+      expect(Discordrb::Events.matches_all(1, 0) { |a, e| a != e }).to be_truthy
+      expect(Discordrb::Events.matches_all(0, 1) { |a, e| a != e }).to be_truthy
+      expect(Discordrb::Events.matches_all(0, 0) { |a, e| a != e }).to be_falsy
     end
 
     it 'should return the opposite results for negated arguments' do
-      Discordrb::Events.matches_all(not!(:a), :e) { true }.should be_falsy
-      Discordrb::Events.matches_all(not!(:a), :e) { 1 }.should be_falsy
-      Discordrb::Events.matches_all(not!(:a), :e) { 0 }.should be_falsy
-      Discordrb::Events.matches_all(not!(:a), :e) { 'string' }.should be_falsy
-      Discordrb::Events.matches_all(not!(:a), :e) { false }.should_not be_falsy
-      Discordrb::Events.matches_all(not!(:a), :e) { nil }.should be_truthy
-      Discordrb::Events.matches_all(not!(:a), :e) { false }.should be_truthy
-      Discordrb::Events.matches_all(not!(:a), :e) { 0 }.should_not be_truthy
-      Discordrb::Events.matches_all(not!(1), 1) { |a, e| a == e }.should be_falsy
-      Discordrb::Events.matches_all(not!(1), 0) { |a, e| a == e }.should be_truthy
-      Discordrb::Events.matches_all(not!(0), 1) { |a, e| a == e }.should be_truthy
-      Discordrb::Events.matches_all(not!(0), 0) { |a, e| a == e }.should be_falsy
-      Discordrb::Events.matches_all(not!(1), 1) { |a, e| a != e }.should be_truthy
-      Discordrb::Events.matches_all(not!(1), 0) { |a, e| a != e }.should be_falsy
-      Discordrb::Events.matches_all(not!(0), 1) { |a, e| a != e }.should be_falsy
-      Discordrb::Events.matches_all(not!(0), 0) { |a, e| a != e }.should be_truthy
+      expect(Discordrb::Events.matches_all(not!(:a), :e) { true }).to be_falsy
+      expect(Discordrb::Events.matches_all(not!(:a), :e) { 1 }).to be_falsy
+      expect(Discordrb::Events.matches_all(not!(:a), :e) { 0 }).to be_falsy
+      expect(Discordrb::Events.matches_all(not!(:a), :e) { 'string' }).to be_falsy
+      expect(Discordrb::Events.matches_all(not!(:a), :e) { false }).to_not be_falsy
+      expect(Discordrb::Events.matches_all(not!(:a), :e) { nil }).to be_truthy
+      expect(Discordrb::Events.matches_all(not!(:a), :e) { false }).to be_truthy
+      expect(Discordrb::Events.matches_all(not!(:a), :e) { 0 }).to_not be_truthy
+      expect(Discordrb::Events.matches_all(not!(1), 1) { |a, e| a == e }).to be_falsy
+      expect(Discordrb::Events.matches_all(not!(1), 0) { |a, e| a == e }).to be_truthy
+      expect(Discordrb::Events.matches_all(not!(0), 1) { |a, e| a == e }).to be_truthy
+      expect(Discordrb::Events.matches_all(not!(0), 0) { |a, e| a == e }).to be_falsy
+      expect(Discordrb::Events.matches_all(not!(1), 1) { |a, e| a != e }).to be_truthy
+      expect(Discordrb::Events.matches_all(not!(1), 0) { |a, e| a != e }).to be_falsy
+      expect(Discordrb::Events.matches_all(not!(0), 1) { |a, e| a != e }).to be_falsy
+      expect(Discordrb::Events.matches_all(not!(0), 0) { |a, e| a != e }).to be_truthy
     end
 
     it 'should find one correct element inside arrays' do
-      Discordrb::Events.matches_all([1, 2, 3], 1) { |a, e| a == e }.should be_truthy
-      Discordrb::Events.matches_all([1, 2, 3], 2) { |a, e| a == e }.should be_truthy
-      Discordrb::Events.matches_all([1, 2, 3], 3) { |a, e| a == e }.should be_truthy
-      Discordrb::Events.matches_all([1, 2, 3], 4) { |a, e| a != e }.should be_truthy
+      expect(Discordrb::Events.matches_all([1, 2, 3], 1) { |a, e| a == e }).to be_truthy
+      expect(Discordrb::Events.matches_all([1, 2, 3], 2) { |a, e| a == e }).to be_truthy
+      expect(Discordrb::Events.matches_all([1, 2, 3], 3) { |a, e| a == e }).to be_truthy
+      expect(Discordrb::Events.matches_all([1, 2, 3], 4) { |a, e| a != e }).to be_truthy
     end
 
     it 'should return false when nothing matches inside arrays' do
-      Discordrb::Events.matches_all([1, 2, 3], 4) { |a, e| a == e }.should be_falsy
+      expect(Discordrb::Events.matches_all([1, 2, 3], 4) { |a, e| a == e }).to be_falsy
     end
 
     it 'should return the respective opposite results for negated arrays' do
-      Discordrb::Events.matches_all(not!([1, 2, 3]), 1) { |a, e| a == e }.should be_falsy
-      Discordrb::Events.matches_all(not!([1, 2, 3]), 2) { |a, e| a == e }.should be_falsy
-      Discordrb::Events.matches_all(not!([1, 2, 3]), 3) { |a, e| a == e }.should be_falsy
-      Discordrb::Events.matches_all(not!([1, 2, 3]), 4) { |a, e| a != e }.should be_falsy
-      Discordrb::Events.matches_all(not!([1, 2, 3]), 4) { |a, e| a == e }.should be_truthy
+      expect(Discordrb::Events.matches_all(not!([1, 2, 3]), 1) { |a, e| a == e }).to be_falsy
+      expect(Discordrb::Events.matches_all(not!([1, 2, 3]), 2) { |a, e| a == e }).to be_falsy
+      expect(Discordrb::Events.matches_all(not!([1, 2, 3]), 3) { |a, e| a == e }).to be_falsy
+      expect(Discordrb::Events.matches_all(not!([1, 2, 3]), 4) { |a, e| a != e }).to be_falsy
+      expect(Discordrb::Events.matches_all(not!([1, 2, 3]), 4) { |a, e| a == e }).to be_truthy
     end
   end
 
@@ -109,7 +109,7 @@ describe Discordrb::Events do
   describe Discordrb::Events::TrueEventHandler do
     describe 'matches?' do
       it 'should return true' do
-        Discordrb::Events::TrueEventHandler.new({}, nil).matches?(nil).should == true
+        expect(Discordrb::Events::TrueEventHandler.new({}, nil).matches?(nil)).to eq true
       end
 
       it 'should always call the block given' do
@@ -117,7 +117,7 @@ describe Discordrb::Events do
         Discordrb::Events::TrueEventHandler.new({}, proc { count += 1 }).match(nil)
         Discordrb::Events::TrueEventHandler.new({}, proc { count += 2 }).match(1)
         Discordrb::Events::TrueEventHandler.new({}, proc do |e|
-          e.should eq(1)
+          expect(e).to eq(1)
           count += 4
         end).match(1)
         Discordrb::Events::TrueEventHandler.new({ a: :b }, proc { count += 8 }).match(1)
