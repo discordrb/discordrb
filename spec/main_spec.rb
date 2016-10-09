b7d8232972baceb63cc5d8b1da40b85eb2957e00
meew0 <blactbt@live.de>
2016-10-09 13:38:25 +0200

    Use expect syntax exclusively in main_spec

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -3,16 +3,16 @@ require 'discordrb'
 describe Discordrb do
   it 'should split messages correctly' do
     split = Discordrb.split_message('a' * 5234)
-    split.should eq(['a' * 2000, 'a' * 2000, 'a' * 1234])
+    expect(split).to eq(['a' * 2000, 'a' * 2000, 'a' * 1234])
 
     # regression test
     # there had been an issue where this would have raised an error,
     # and (if it hadn't raised) produced incorrect results
     split = Discordrb.split_message(('a' * 800 + "\n") * 6)
-    split.should eq([
-                      'a' * 800 + "\n" + 'a' * 800 + "\n",
-                      'a' * 800 + "\n" + 'a' * 800 + "\n",
-                      'a' * 800 + "\n" + 'a' * 800
-                    ])
+    expect(split).to eq([
+                          'a' * 800 + "\n" + 'a' * 800 + "\n",
+                          'a' * 800 + "\n" + 'a' * 800 + "\n",
+                          'a' * 800 + "\n" + 'a' * 800
+                        ])
   end
 end
