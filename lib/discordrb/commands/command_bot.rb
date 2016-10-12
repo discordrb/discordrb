f73dd2563f5695e0347afea9118f7a8eef3fbd03
Jonas Osborn <jonas.w.osborn@gmail.com>
2016-10-12 12:14:28 +0100

    Move channel check to stop it interfering with unrecognized commands

--- lib/discordrb/commands/command_bot.rb
+++ lib/discordrb/commands/command_bot.rb
@@ -176,11 +176,12 @@ module Discordrb::Commands
       debug("Executing command #{name} with arguments #{arguments}")
       return unless @commands
       command = @commands[name]
-      return unless channels?(event.channel, command.attributes[:channels])
       unless command
         event.respond @attributes[:command_doesnt_exist_message].gsub('%command%', name.to_s) if @attributes[:command_doesnt_exist_message]
         return
       end
+      return unless channels?(event.channel, command.attributes[:channels])
+      if permission?(event.author, command.attributes[:permission_level], event.server) &&
       if (check_permissions &&
          permission?(event.author, command.attributes[:permission_level], event.server) &&
          required_permissions?(event.author, command.attributes[:required_permissions], event.channel) &&
