f188eee052a3334647a52d9fe0dcbec7440cd539
meew0 <blactbt@live.de>
2016-10-17 19:19:15 +0200

    Apply check_permissions checks to channels? ones

--- lib/discordrb/commands/command_bot.rb
+++ lib/discordrb/commands/command_bot.rb
@@ -180,12 +180,13 @@ module Discordrb::Commands
       debug("Executing command #{name} with arguments #{arguments}")
       return unless @commands
       command = @commands[name]
-      return unless channels?(event.channel, @attributes[:channels]) ||
+      return unless !check_permissions || channels?(event.channel, @attributes[:channels]) ||
                     (command && !command.attributes[:channels].nil?)
       unless command
         event.respond @attributes[:command_doesnt_exist_message].gsub('%command%', name.to_s) if @attributes[:command_doesnt_exist_message]
         return
       end
+      return unless !check_permissions || channels?(event.channel, command.attributes[:channels])
       return unless channels?(event.channel, command.attributes[:channels])
       if permission?(event.author, command.attributes[:permission_level], event.server) &&
       if (check_permissions &&
