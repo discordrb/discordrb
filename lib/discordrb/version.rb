1e293e8a43353dca3a0d99cfc1b55f7091ffe8c7
meew0 <blactbt@live.de>
2016-10-20 10:57:52 +0200

    3.1.0

--- lib/discordrb/version.rb
+++ lib/discordrb/version.rb
@@ -3,5 +3,5 @@
 # Discordrb and all its functionality, in this case only the version.
 module Discordrb
   # The current version of discordrb.
-  VERSION = '3.0.2'.freeze
+  VERSION = '3.1.0'.freeze
 end
