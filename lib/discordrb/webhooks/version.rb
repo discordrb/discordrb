d97f50891f461b364906a50f7dfa4be95d5df046
meew0 <blactbt@live.de>
2016-10-20 13:42:53 +0200

    :anchor: Declare the discordrb module separately in webhooks/version

--- lib/discordrb/webhooks/version.rb
+++ lib/discordrb/webhooks/version.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 # Webhook support for discordrb
-module Discordrb::Webhooks
-  # The current version of discordrb-webhooks.
-  VERSION = '0.1.0'.freeze
+module Discordrb
+  module Webhooks
+    # The current version of discordrb-webhooks.
+    VERSION = '0.1.0'.freeze
+  end
 end
