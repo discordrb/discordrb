discordrb/discordrb

commit 69939e37a7f846298bb81d2337bcdba1b74fc51a

Ignore GUILD_DELETEs for servers becoming unavailable

lib/discordrb/bot.rb
@@ -1027,6 +1027,11 @@ module Discordrb
       when :GUILD_DELETE
         delete_guild(data)
 
+        if d['unavailable'].is_a? TrueClass
+          LOGGER.warn("Server #{d['id']} is unavailable due to an outage!")
+          return # Don't raise an event
+        end
+
         event = ServerDeleteEvent.new(data, self)
         raise_event(event)
       else

commit 5afa0512573bab31ba252df6ac5c296fa5fc1039

Fix bug introduced in 69939e3
No idea how you were able to miss this @meew0

lib/discordrb/bot.rb
@@ -1093,8 +1093,8 @@ module Discordrb
       when :GUILD_DELETE
         delete_guild(data)
 
-        if d['unavailable'].is_a? TrueClass
-          LOGGER.warn("Server #{d['id']} is unavailable due to an outage!")
+        if data['unavailable'].is_a? TrueClass
+          LOGGER.warn("Server #{data['id']} is unavailable due to an outage!")
           return # Don't raise an event
         end
 