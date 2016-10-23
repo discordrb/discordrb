0b9d022b056fda22c2dbc558db7873615e624ad4
meew0 <blactbt@live.de>
2016-10-23 23:48:51 +0200

    :anchor: Require all the webhook files in webhooks.rb

--- lib/discordrb/webhooks.rb
+++ lib/discordrb/webhooks.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require 'discordrb/webhooks/version'
+require 'discordrb/webhooks/embeds'
+require 'discordrb/webhooks/client'
+require 'discordrb/webhooks/builder'
 
 module Discordrb
   # Webhook client
