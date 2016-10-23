8a7c1d00ee006f2367467b27f2235badc2a04326
meew0 <blactbt@live.de>
2016-10-23 23:50:32 +0200

    :anchor: Write a spec that Embed#colour= accepts decimal format

--- spec/webhooks_spec.rb
+++ spec/webhooks_spec.rb
@@ -9,5 +9,15 @@ describe Discordrb::Webhooks do
 
       expect(embed.fields.length).to eq 1
     end
+
+    describe '#colour=' do
+      it 'should accept colours in decimal format' do
+        embed = Discordrb::Webhooks::Embed.new
+        colour = 1234
+
+        embed.colour = colour
+        expect(embed.colour).to eq colour
+      end
+    end
   end
 end
