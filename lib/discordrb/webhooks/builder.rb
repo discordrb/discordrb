040a7637163bc08e12d116b83a2a63eca6d54ab5
meew0 <blactbt@live.de>
2016-10-23 20:12:16 +0200

    :anchor: Create an initializer for Builder that initializes everything with default values

--- lib/discordrb/webhooks/builder.rb
+++ lib/discordrb/webhooks/builder.rb
@@ -1,5 +1,13 @@
 module Discordrb::Webhooks
   # A class that acts as a builder for a webhook message object.
   class Builder
+    def initialize(content: '', username: nil, avatar_url: nil, tts: false, file: nil, embeds: [])
+      @content = content
+      @username = username
+      @avatar_url = avatar_url
+      @tts = tts
+      @file = file
+      @embeds = embeds
+    end
   end
 end
