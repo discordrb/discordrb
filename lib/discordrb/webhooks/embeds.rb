80cc1c60cae27fdbb4301ada08bc0cef25764c72
meew0 <blactbt@live.de>
2016-10-23 22:37:55 +0200

    :anchor: Add a class for webhook embeds

---
+++ lib/discordrb/webhooks/embeds.rb
@@ -0,0 +1,6 @@
+module Discordrb::Webhooks
+  # An embed is a multipart-style attachment to a webhook message that can have a variety of different purposes and
+  # appearances.
+  class Embed
+  end
+end
