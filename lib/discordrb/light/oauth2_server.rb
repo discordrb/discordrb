2fb8cc4736b6cd418fb97226cd1fb5fd2e5ee24f
meew0 <blactbt@live.de>
2016-10-28 00:54:35 +0200

    Start an OAuth2Server class to act as a tiny redirect URL target

---
+++ lib/discordrb/light/oauth2_server.rb
@@ -0,0 +1,7 @@
+module Discordrb::Light
+  # A utility class that provides a local HTTP server to act as a redirect URI
+  # in OAuth2 applications. Useful for RPC, or when you're making a script that
+  # wants to do a simple OAuth2-style request in a zeroconf manner.
+  class OAuth2Server
+  end
+end
