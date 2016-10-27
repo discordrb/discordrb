58626704fdf3706cae82f6fa4beb960b3ecb59b0
meew0 <blactbt@live.de>
2016-10-27 22:22:05 +0200

    Add an initializer to the RPC client

--- lib/discordrb/rpc/client.rb
+++ lib/discordrb/rpc/client.rb
@@ -1,5 +1,9 @@
 module Discordrb::RPC
   # Client for Discord's RPC protocol.
   class Client
+    def initialize(client_id, origin)
+      @client_id = client_id
+      @origin = origin
+    end
   end
 end
