dcc5e31c7b1e2b5eacc9869c954de811cf7f6619
meew0 <blactbt@live.de>
2016-10-28 00:56:24 +0200

    Write a basic initializer for OAuth2Server

--- lib/discordrb/light/oauth2_server.rb
+++ lib/discordrb/light/oauth2_server.rb
@@ -3,5 +3,10 @@ module Discordrb::Light
   # in OAuth2 applications. Useful for RPC, or when you're making a script that
   # wants to do a simple OAuth2-style request in a zeroconf manner.
   class OAuth2Server
+    # Creates a new OAuth2 server, without any startup or running being done.
+    # @param port [Integer] The port to listen on.
+    def initialize(port)
+      @port = port
+    end
   end
 end
