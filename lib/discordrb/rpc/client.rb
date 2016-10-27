d02491292cec6c0051346c522cb780186201f322
meew0 <blactbt@live.de>
2016-10-27 22:46:10 +0200

    Send the origin over the WS headers

--- lib/discordrb/rpc/client.rb
+++ lib/discordrb/rpc/client.rb
@@ -11,12 +11,17 @@ module Discordrb::RPC
     def connect
       url = 'wss://discordapp.io:6463/?v=1'
 
+      headers = {
+        origin: @origin
+      }
+
       @ws = Discordrb::WebSocket.new(
         url,
         method(:websocket_open),
         method(:websocket_message),
         proc { |e| Discordrb::LOGGER.error "RPCWS error: #{e}" },
-        proc { |e| Discordrb::LOGGER.warn "RPCWS close: #{e}" }
+        proc { |e| Discordrb::LOGGER.warn "RPCWS close: #{e}" },
+        headers: headers
       )
     end
 
