9d3cc84097b60e3e76bb36445c3b374ee2bbdd30
meew0 <blactbt@live.de>
2016-10-27 22:43:08 +0200

    Add some basic connection logic

--- lib/discordrb/rpc/client.rb
+++ lib/discordrb/rpc/client.rb
@@ -1,9 +1,23 @@
+require 'discordrb/websocket'
+
 module Discordrb::RPC
   # Client for Discord's RPC protocol.
   class Client
     def initialize(client_id, origin)
       @client_id = client_id
       @origin = origin
+    end
+
+    def connect
+      url = 'wss://discordapp.io:6463/?v=1'
+
+      @ws = Discordrb::WebSocket.new(
+        url,
+        method(:websocket_open),
+        method(:websocket_message),
+        proc { |e| Discordrb::Logger.error "RPCWS error: #{e}" },
+        proc { |e| Discordrb::Logger.warn "RPCWS close: #{e}" }
+      )
     end
   end
 end
