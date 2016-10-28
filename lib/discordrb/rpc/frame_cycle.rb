5b0b9f210ca1feecdc12e003e09577b2b1bb18ce
meew0 <blactbt@live.de>
2016-10-28 19:45:28 +0200

    Add an initializer to FrameCycle

--- lib/discordrb/rpc/frame_cycle.rb
+++ lib/discordrb/rpc/frame_cycle.rb
@@ -1,5 +1,10 @@
 module Discordrb::RPC
   # Represents a cycle of one RPC request to its response.
   class FrameCycle
+    # Creates a new frame cycle.
+    # @param nonce [String] The nonce to uniquely identify this cycle.
+    def initialize(nonce)
+      @nonce = nonce
+    end
   end
 end
