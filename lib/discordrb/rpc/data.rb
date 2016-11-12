868c3856c28cc939b256a7b972e6062eb27462d1
meew0 <blactbt@live.de>
2016-11-12 14:07:01 +0100

    Create a RPCUser class

--- lib/discordrb/rpc/data.rb
+++ lib/discordrb/rpc/data.rb
@@ -1,5 +1,25 @@
 require 'json'
 
+require 'discordrb/data'
+
 # Support for Discord's RPC protocol
 module Discordrb::RPC
+  # Represents a user as sent over RPC.
+  class RPCUser
+    include Discordrb::IDObject
+    include Discordrb::UserAttributes
+
+    # @!visibility private
+    def initialize(data, bot)
+      @bot = bot
+
+      @username = data['username']
+      @id = data['id'].to_i
+      @discriminator = data['discriminator']
+      @avatar_id = data['avatar']
+
+      @bot_account = false
+      @bot_account = true if data['bot']
+    end
+  end
 end
