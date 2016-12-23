1ee1ac58a36d0352b8872fc7907f49e9cd549dd0
meew0 <blactbt@live.de>
2016-12-23 15:58:22 +0100

    Create a RawEvent class for raw dispatches in general

--- lib/discordrb/events/raw.rb
+++ lib/discordrb/events/raw.rb
@@ -4,4 +4,14 @@ require 'discordrb/events/generic'
 
 # Event classes and handlers
 module Discordrb::Events
+  # Event raised when any dispatch is received
+  class RawEvent < Event
+    # @return [Symbol] the type of this dispatch.
+    attr_reader :type
+    alias_method :t, :type
+
+    # @return [Hash] the data of this dispatch.
+    attr_reader :data
+    alias_method :d, :data
+  end
 end
