cd2b9153dbb2da565b4c104daa8ce4f4ab50012d
meew0 <blactbt@live.de>
2016-12-23 16:23:11 +0100

    Reverse the order of the aliasing in RawEventHandler#matches?

--- lib/discordrb/events/raw.rb
+++ lib/discordrb/events/raw.rb
@@ -28,7 +28,7 @@ module Discordrb::Events
       return false unless event.is_a? RawEvent
 
       [
-        matches_all(@attributes[:t] || @attributes[:type], event.type) do |a, e|
+        matches_all(@attributes[:type] || @attributes[:t], event.type) do |a, e|
           if a.is_a? Regexp
             # 24: update to matches?
             match = a.match(e)
