37f1897712f299c6e948f5298193f4329e6c2b7c
meew0 <blactbt@live.de>
2016-12-25 14:42:19 +0100

    Add an initializer to ReactionEvent

--- lib/discordrb/events/reactions.rb
+++ lib/discordrb/events/reactions.rb
@@ -6,5 +6,13 @@ require 'discordrb/data'
 module Discordrb::Events
   # Generic superclass for events about adding and removing reactions
   class ReactionEvent
+    def initialize(data, bot)
+      @bot = bot
+
+      @emoji = Discordrb::Emoji.new(data['emoji'], bot, nil)
+      @user_id = data['user_id'].to_i
+      @message_id = data['message_id'].to_i
+      @channel_id = data['channel_id'].to_i
+    end
   end
 end
