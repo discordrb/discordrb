0daa0d13d3562995d02bafd808f8081657785df8
meew0 <blactbt@live.de>
2016-12-28 13:16:46 +0100

    Check for the correct methods on the builder object
    This is for convenience so people don't accidentally pass wrong stuff into there.

--- lib/discordrb/webhooks/client.rb
+++ lib/discordrb/webhooks/client.rb
@@ -41,6 +41,9 @@ module Discordrb::Webhooks
     #   end
     # @return [RestClient::Response] the response returned by Discord.
     def execute(builder = nil, wait = false)
+      raise TypeError, 'builder needs to be nil or like a Discordrb::Webhooks::Builder!' unless
+        builder.respond_to?(:file) && builder.respond_to?(:to_multipart_hash) || builder.respond_to?(:to_json_hash) || builder.nil?
+
       builder ||= Builder.new
 
       yield builder if block_given?
