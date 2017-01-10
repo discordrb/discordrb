3f8d60df0362e00daa597677d9530c8676c56412
LikeLakers2 <likelakers24@gmail.com>
2017-01-10 13:20:17 -0500

    Remove that whitespace so Rubocop doesn't yell

--- lib/discordrb/api.rb
+++ lib/discordrb/api.rb
@@ -136,7 +136,7 @@ module Discordrb::API
   def widget_url(server_id, style = 'shield')
     "#{api_base}/guilds/#{server_id}/widget.png?style=#{style}"
   end
-  
+
   # Make a splash URL from server and splash IDs
   def splash_url(server_id, splash_id)
     "https://cdn.discordapp.com/splashes/#{server_id}/#{splash_id}.jpg"
