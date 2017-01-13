9a6a38130c3b39f89822f47e1c37b38b246984bf
meew0 <blactbt@live.de>
2017-01-14 00:34:27 +0100

    Add some basic specs for bulk_delete

---
+++ spec/data_spec.rb
@@ -0,0 +1,28 @@
+require 'discordrb'
+require 'mock/api_mock'
+
+using APIMock
+
+module Discordrb
+  describe Channel do
+    let(:data) { load_data_file(:text_channel) }
+
+    subject(:channel) do
+      bot = double('bot')
+      allow(bot).to receive(:token) { 'fake token' }
+      described_class.new(data, bot, double('server'))
+    end
+
+    describe '#delete_messages' do
+      it 'should fail with more than 100 messages' do
+        messages = [*1..101]
+        expect { channel.delete_messages(messages) }.to raise_error(ArgumentError)
+      end
+
+      it 'should fail with less than 2 messages' do
+        messages = [1]
+        expect { channel.delete_messages(messages) }.to raise_error(ArgumentError)
+      end
+    end
+  end
+end
