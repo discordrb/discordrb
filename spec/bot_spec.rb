daec33e2f74c0a244652deecad3513d19ce481a9
meew0 <blactbt@live.de>
2017-01-14 01:21:22 +0100

    Add the emoji ID and name constants to bot_spec
    There's no need to have them in helpers anymore.

--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -1,12 +1,6 @@
 require 'discordrb'
-require 'helpers'
-
-RSpec.configure do |c|
-  c.include Helpers
-end
 
 module Discordrb
-  include Helpers
   describe Bot do
     subject(:bot) do
       described_class.new(token: 'fake_token')
@@ -17,6 +11,17 @@ module Discordrb
     end
 
     let!(:server_id) { server.id }
+
+    # This data is represented in the fixtures (see ./data/emoji)
+    EMOJI1_ID = 10
+    EMOJI1_NAME = 'emoji_name_1'.freeze
+
+    EMOJI2_ID = 11
+    EMOJI2_NAME = 'emoji_name_2'.freeze
+    EDITED_EMOJI_NAME = 'new_emoji_name'.freeze
+
+    EMOJI3_ID = 12
+    EMOJI3_NAME = 'emoji_name_3'.freeze
 
     before do
       bot.instance_variable_set(:@servers, server_id => server)
@@ -60,13 +65,12 @@ module Discordrb
       end
 
       it 'edits an emoji' do
-        emoji_name = 'new_emoji_name'
         data = load_data_file(:emoji, :dispatch_update)
         bot.send(:update_guild_emoji, data)
         emojis = bot.server(server_id).emoji
         emoji = emojis[EMOJI2_ID]
         expect(emojis.size).to eq(2)
-        expect(emoji.name).to eq(emoji_name)
+        expect(emoji.name).to eq(EDITED_EMOJI_NAME)
         expect(emoji.server).to eq(server)
         expect(emoji.roles).to eq([])
       end
