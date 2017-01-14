4c228d153bbda094c9073eb39cfdb8d5cfa3ed66
meew0 <blactbt@live.de>
2017-01-14 01:44:19 +0100

    Spec filtering in fixture_property

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -28,8 +28,13 @@ end
 describe '#fixture_property' do
   fixture :data, [:test]
   fixture_property :data_success, :data, ['success']
+  fixture_property :data_success_str, :data, ['success'], :to_s
 
   it 'should define the test property correctly' do
     expect(data_success).to eq(true)
+  end
+
+  it 'should filter data correctly' do
+    expect(data_success_str).to eq('true')
   end
 end
