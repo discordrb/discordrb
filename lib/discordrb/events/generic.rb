feb94b867bb370bc5af33937932d3b8ead438689
meew0 <blactbt@live.de>
2017-01-24 16:55:21 +0100

    Reimplement ActiveSupport's #delegate internally
     It is now a protected static method on Event, only to be used by sub-events. This way we can get rid of the ActiveSupport dependency which was causing a bit of trouble.

--- lib/discordrb/events/generic.rb
+++ lib/discordrb/events/generic.rb
@@ -55,6 +55,24 @@ module Discordrb::Events
   class Event
     # @return [Bot] the bot used to initialize this event.
     attr_reader :bot
+
+    class << self
+      protected
+
+      # Delegates a list of methods to a particular object. This is essentially a reimplementation of ActiveSupport's
+      # `#delegate`, but without the overhead provided by the rest. Used in subclasses of `Event` to delegate properties
+      # on events to properties on data objects.
+      # @param methods [Array<Symbol>] The methods to delegate.
+      # @param hash [Hash<Symbol => Symbol>] A hash with one `:to` key and the value the method to be delegated to.
+      def delegate(*methods, hash)
+        methods.each do |e|
+          define_method(e) do
+            object = __send__(hash[:to])
+            object.__send__(e)
+          end
+        end
+      end
+    end
   end
 
   # Generic event handler that can be extended
