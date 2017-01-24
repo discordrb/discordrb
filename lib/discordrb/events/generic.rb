42f39d8880c640dac39717a70f5d1122d30fab81
meew0 <blactbt@live.de>
2017-01-24 20:31:21 +0100

    Remove the activesupport include from generic.rb

--- lib/discordrb/events/generic.rb
+++ lib/discordrb/events/generic.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'active_support/core_ext/module'
-
 # Events used by discordrb
 module Discordrb::Events
   # A negated object, used to not match something in event parameters.
