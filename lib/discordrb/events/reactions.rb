de9cc00d736f094c43cb4de9e22d55dfd643b115
Daniel Worrall <Daniel.Worrall@hotmail.co.uk>
2017-05-29 06:36:40 +0100

    Fixed typo in docs for ReactionEvent#server

--- lib/discordrb/events/reactions.rb
+++ lib/discordrb/events/reactions.rb
@@ -42,7 +42,7 @@ module Discordrb::Events
       @channel ||= @bot.channel(@channel_id)
     end
 
-    # @return [Server, nil] the server that was reacted in. If this channel is a PM channel, it will be nil.
+    # @return [Server, nil] the server that was reacted in. If reacted in a PM channel, it will be nil.
     def server
       @server ||= @channel.server
     end
