477566e58508a2e82e1ae184c69cc341186ac633
Zac Nowicki <zachnowicki@gmail.com>
2017-08-05 23:17:55 -0400

    add an example showing how to manually register an await

--- examples/awaits.rb
+++ examples/awaits.rb
@@ -41,8 +41,39 @@ bot.message(start_with: '!game') do |event|
   event.respond 'Guess a number between 1 and 10..'
 end
 
+# Above we used the provided User#await method to easily set up
+# an await for a follow-up message from a user.
+# We can also manually register an await for specific kinds of events.
+# Here, we'll write a command that shows the current time and allows
+# the user to delete the message with a reaction.
+
+# the unicode ":x:" emoji
+CROSS_MARK = "\u274c".freeze
+
+bot.message(content: '!time') do |event|
+  # Send a message, and store a reference to it that we can issue a delete request later
+  message = event.respond "The current time is: #{Time.now.strftime('%F %T %Z')}"
+
+  # React to the message to give a user an easy "button" to press
+  message.react CROSS_MARK
+
+  # Add an await for a ReactionAddEvent, that will only trigger for reactions
+  # that match our CROSS_MARK emoji.
+  bot.add_await(:"delete_#{message.id}", Discordrb::Events::ReactionAddEvent, emoji: CROSS_MARK) do |reaction_event|
+    # Since this code will run on every CROSS_MARK reaction, it might not
+    # be on our time message we sent earlier. We use `next` to skip the rest
+    # of the block unless it was our message that reacted to.
+    next true unless reaction_event.message.id == message.id
+
+    # Delete the matching message.
+    message.delete
+  end
+end
+
 # Connect to Discord
 bot.run
 
 # For more details about Awaits, see:
 # http://www.rubydoc.info/gems/discordrb/Discordrb/Await
+# For a list of events you can use to await for, see:
+# http://www.rubydoc.info/gems/discordrb/Discordrb/Events
