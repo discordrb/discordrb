09f1e59f892411cf11812ab638ebe5c3c788a2bc
PixeL <officialpixelinc@gmail.com>
2017-11-21 00:41:41 -0600

    So much space for Activities!

--- lib/discordrb/bot.rb
+++ lib/discordrb/bot.rb
@@ -445,25 +445,25 @@ module Discordrb
 
     # Updates presence status.
     # @param status [String] The status the bot should show up as. Can be `online`, `dnd`, `idle`, or `invisible`
-    # @param game [String, nil] The name of the game to be played/watched/listened to/stream name on the stream.
+    # @param activity [String, nil] The name of the activity to be played/watched/listened to/stream name on the stream.
     # @param url [String, nil] The Twitch URL to display as a stream. nil for no stream.
     # @param since [Integer] When this status was set.
     # @param afk [true, false] Whether the bot is AFK.
-    # @param game_type [Integer] The type of game status to display. Can be 0 (Playing), 1 (Streaming), 2 (Listening), 3 (Watching)
+    # @param activity_type [Integer] The type of activity status to display. Can be 0 (Playing), 1 (Streaming), 2 (Listening), 3 (Watching)
     # @see Gateway#send_status_update
-    def update_status(status, game, url, since = 0, afk = false, game_type = 0)
+    def update_status(status, activity, url, since = 0, afk = false, activity_type = 0)
       gateway_check
 
-      @game = game
+      @activity = activity
       @status = status
       @streamurl = url
-      type = url ? 1 : game_type
+      type = url ? 1 : activity_type
 
-      game_obj = game || url ? { 'name' => game, 'url' => url, 'type' => type } : nil
-      @gateway.send_status_update(status, since, game_obj, afk)
+      activity_obj = activity || url ? { 'name' => activity, 'url' => url, 'type' => type } : nil
+      @gateway.send_status_update(status, since, activity_obj, afk)
 
       # Update the status in the cache
-      profile.update_presence('status' => status.to_s, 'game' => game_obj)
+      profile.update_presence('status' => status.to_s, 'game' => activity_obj)
     end
 
     # Sets the currently playing game to the specified game.
@@ -508,7 +508,7 @@ module Discordrb
     # Sets status to online.
     def online
       gateway_check
-      update_status(:online, @game, @streamurl)
+      update_status(:online, @activity, @streamurl)
     end
 
     alias_method :on, :online
@@ -516,7 +516,7 @@ module Discordrb
     # Sets status to idle.
     def idle
       gateway_check
-      update_status(:idle, @game, nil)
+      update_status(:idle, @activity, nil)
     end
 
     alias_method :away, :idle
@@ -524,13 +524,13 @@ module Discordrb
     # Sets the bot's status to DnD (red icon).
     def dnd
       gateway_check
-      update_status(:dnd, @game, nil)
+      update_status(:dnd, @activity, nil)
     end
 
     # Sets the bot's status to invisible (appears offline).
     def invisible
       gateway_check
-      update_status(:invisible, @game, nil)
+      update_status(:invisible, @activity, nil)
     end
 
     # Sets debug mode. If debug mode is on, many things will be outputted to STDOUT.
