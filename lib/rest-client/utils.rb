b291980f2b04ed907ef06f48a3e3e036f85fd126
swarley <matthew.b.carey@gmail.com>
2018-03-31 20:10:36 -0400

    More formatting changes

--- lib/rest-client/utils.rb
+++ lib/rest-client/utils.rb
@@ -1,10 +1,12 @@
 if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.5') && ENV['OS'] == 'Windows_NT'
-    
-  # @note In Ruby versions greater than 2.4 on Windows, Discordrb's
-  # environment does not play nicely with the use of enumerators in
-  # this specific function. The exact reason is unknown, but changing the use of
-  # `Enumerator#next` into iteration via `Array#shift` prevents a silent crash.
+  
+  # @note This fix in particular addresses an issue within RestClient, confirmed
+  # in RestClient 2.0.2 but only within the Discordrb environment.
   module RestClient
+    # @note In Ruby versions greater than 2.4 on Windows, Discordrb's
+    # environment does not play nicely with the use of enumerators in
+    # this specific function. The exact reason is unknown, but changing the use of
+    # `Enumerator#next` into iteration via `Array#shift` prevents a silent crash.
     module Utils
       def self.cgi_parse_header(line)
         parts = _cgi_parseparam(';' + line).to_a
@@ -13,7 +15,7 @@ if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.5') && ENV['OS'] == 'Wi
 
         while (p = parts.shift)
           i = p.index('=')
-          next if !i
+          next unless i
 
           name = p[0...i].strip.downcase
           value = p[i + 1..-1].strip
