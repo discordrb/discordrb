4c57926f26cfa86cb722961d992268ccc52f3d19
swarley <matthew.b.carey@gmail.com>
2018-03-31 20:04:36 -0400

    Fixing formatting for travis offenses

--- lib/rest-client/utils.rb
+++ lib/rest-client/utils.rb
@@ -1,9 +1,9 @@
-# @note In Ruby versions greater than 2.4 on Windows, Discordrb's 
-# environment does not play nicely with the use of enumerators in
-# this specific function. The exact reason is unknown, but changing the use of
-# `Enumerator#next` into iteration via `Array#shift` prevents a silent crash.
-
 if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.5') && ENV['OS'] == 'Windows_NT'
+    
+  # @note In Ruby versions greater than 2.4 on Windows, Discordrb's
+  # environment does not play nicely with the use of enumerators in
+  # this specific function. The exact reason is unknown, but changing the use of
+  # `Enumerator#next` into iteration via `Array#shift` prevents a silent crash.
   module RestClient
     module Utils
       def self.cgi_parse_header(line)
@@ -13,17 +13,17 @@ if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.5') && ENV['OS'] == 'Wi
 
         while (p = parts.shift)
           i = p.index('=')
-          if i
-            name = p[0...i].strip.downcase
-            value = p[i+1..-1].strip
-            if value.length >= 2 && value[0] == '"' && value[-1] == '"'
-              value = value[1...-1]
-              value = value.gsub('\\\\', '\\').gsub('\\"', '"')
-            end
-            pdict[name] = value
+          next if !i
+
+          name = p[0...i].strip.downcase
+          value = p[i + 1..-1].strip
+          if value.length >= 2 && value[0] == '"' && value[-1] == '"'
+            value = value[1...-1]
+            value = value.gsub('\\\\', '\\').gsub('\\"', '"')
           end
+          pdict[name] = value
         end
-  
+
         [key, pdict]
       end
     end
