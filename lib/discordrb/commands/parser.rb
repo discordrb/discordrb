10130078ec661c3eae8f8bdb19fc3fd4d9fde7ae
unleashy <lakeavenger@gmail.com>
2018-07-30 15:48:35 -0300

    Fixes issue #501
    Ensures comamnd chains handle nil results from
    CommandBot#execute_command properly; breaking out of the chain
    execution loop in case that happens.

--- lib/discordrb/commands/parser.rb
+++ lib/discordrb/commands/parser.rb
@@ -243,6 +243,9 @@ module Discordrb::Commands
 
         # Finally execute the command
         prev = @bot.execute_command(command_name.to_sym, event, arguments, split_chain.length > 1 || @subchain)
+        
+        # Stop chain if execute_command failed (maybe the command doesn't exist, or permissions failed, etc.)
+        break unless prev
       end
 
       prev
