c7a6c923e079c3dd44fa19914cc256d6ecaf2747
Zac Nowicki <zachnowicki@gmail.com>
2018-09-04 19:23:40 +0000

    Add frozen_string_literal magic comment to files where missing

--- lib/discordrb/webhooks/embeds.rb
+++ lib/discordrb/webhooks/embeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Discordrb::Webhooks
   # An embed is a multipart-style attachment to a webhook message that can have a variety of different purposes and
   # appearances.
