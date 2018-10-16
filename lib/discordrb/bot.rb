a4a9f925f263406e4ca8c5256d28ec8df0954e35
Connor Shea <connor.james.shea@gmail.com>
2018-10-16 16:12:04 -0600

    Appease the Rubocop.

--- lib/discordrb/bot.rb
+++ lib/discordrb/bot.rb
@@ -670,11 +670,10 @@ module Discordrb
     def gateway_check
       raise "A gateway connection is necessary to call this method! You'll have to do it inside any event (e.g. `ready`) or after `bot.run :async`." unless connected?
 
-      if @unavailable_servers && @unavailable_servers > 0
-        # There are servers that are unavailable.
-        LOGGER.warn("#{@unavailable_servers} servers haven't been cached yet.")
-        LOGGER.warn('Servers may be unavailable due to an outage, or your bot is on very large servers that are taking a while to load.')
-      end
+      # Return unless there are servers that are unavailable.
+      return unless @unavailable_servers && @unavailable_servers > 0
+      LOGGER.warn("#{@unavailable_servers} servers haven't been cached yet.")
+      LOGGER.warn('Servers may be unavailable due to an outage, or your bot is on very large servers that are taking a while to load.')
     end
 
     ### ##    ## ######## ######## ########  ##    ##    ###    ##        ######
