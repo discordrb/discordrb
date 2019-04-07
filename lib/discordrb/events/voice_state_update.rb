ca9b801c013350401c11ce63ac9c07fc9b33b9ed
Matthew Carey <matthew.b.carey@gmail.com>
2019-04-06 20:23:39 -0400

    Fixing VoiceStateUpdate matching

--- lib/discordrb/events/voice_state_update.rb
+++ lib/discordrb/events/voice_state_update.rb
@@ -47,28 +47,28 @@ module Discordrb::Events
                end
         end,
         matches_all(@attributes[:mute], event.mute) do |a, e|
-          a == if a.is_a?(TrueClass) || a.is_a?(FalseClass)
+          a == if a.is_a? String
                  e.to_s
                else
                  e
                end
         end,
         matches_all(@attributes[:deaf], event.deaf) do |a, e|
-          a == if a.is_a?(TrueClass) || a.is_a?(FalseClass)
+          a == if a.is_a? String
                  e.to_s
                else
                  e
                end
         end,
         matches_all(@attributes[:self_mute], event.self_mute) do |a, e|
-          a == if a.is_a?(TrueClass) || a.is_a?(FalseClass)
+          a == if a.is_a? String
                  e.to_s
                else
                  e
                end
         end,
         matches_all(@attributes[:self_deaf], event.self_deaf) do |a, e|
-          a == if a.is_a?(TrueClass) || a.is_a?(FalseClass)
+          a == if a.is_a? String
                  e.to_s
                else
                  e
