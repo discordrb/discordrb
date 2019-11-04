624857840664055cdb5a5b54234ea245c7474d4d
Matt Carey <matthew.b.carey@gmail.com>
2019-11-04 18:45:44 -0500

    add commandbot support

--- lib/discordrb/commands/command_bot.rb
+++ lib/discordrb/commands/command_bot.rb
@@ -83,7 +83,9 @@ module Discordrb::Commands
         num_shards: attributes[:num_shards],
         redact_token: attributes.key?(:redact_token) ? attributes[:redact_token] : true,
         ignore_bots: attributes[:ignore_bots],
-        compress_mode: attributes[:compress_mode])
+        compress_mode: attributes[:compress_mode],
+        mobile: attributes[:mobile]
+      )
 
       @prefix = attributes[:prefix]
       @attributes = {
