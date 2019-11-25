discordrb/discordrb

commit 0f74a1e10def06fa82c34a9d517174a496f00657

Change getc to readpartial (#680)

lib/discordrb/gateway.rb
@@ -594,7 +594,7 @@ module Discordrb
           end
 
           # Get some data from the socket
-          recv_data = @socket.getc
+          recv_data = @socket.readpartial(4096)
 
           # Check if we actually got data
           unless recv_data

commit f44dc38a35da2771275cc72a5671adeec463f807

Handle EOFError in websocket loop (#681)
Handles uncaught exception introduced in #680

lib/discordrb/gateway.rb
@@ -591,10 +591,17 @@ module Discordrb
           unless @socket
             LOGGER.warn('Socket is nil in websocket_loop! Reconnecting')
             handle_internal_close('Socket is nil in websocket_loop')
+            next
           end
 
           # Get some data from the socket
-          recv_data = @socket.readpartial(4096)
+          begin
+            recv_data = @socket.readpartial(4096)
+          rescue EOFError
+            @pipe_broken = true
+            handle_internal_close('Socket EOF in websocket_loop')
+            next
+          end
 
           # Check if we actually got data
           unless recv_data