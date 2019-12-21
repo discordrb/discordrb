30f36bc95021fffff178207550375c2e088ad171
Matt Carey <matthew.b.carey@gmail.com>
2019-12-20 23:14:39 -0500

    add command bot intents

--- lib/discordrb/commands/command_bot.rb
+++ lib/discordrb/commands/command_bot.rb
@@ -83,7 +83,9 @@ module Discordrb::Commands
         num_shards: attributes[:num_shards],
         redact_token: attributes.key?(:redact_token) ? attributes[:redact_token] : true,
         ignore_bots: attributes[:ignore_bots],
-        compress_mode: attributes[:compress_mode])
+        compress_mode: attributes[:compress_mode],
+        intents: attributes[:intents]
+      )
 
       @prefix = attributes[:prefix]
       @attributes = {
