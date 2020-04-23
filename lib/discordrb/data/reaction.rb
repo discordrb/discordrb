137d73e856fe570b9d927b3bf5210a4d6f92bfb2
GrapeColor <grapecolor@users.noreply.github.com>
2020-04-23 11:54:46 +0900

    Add emoji and delete id and name attributes.

--- lib/discordrb/data/reaction.rb
+++ lib/discordrb/data/reaction.rb
@@ -10,24 +10,13 @@ module Discordrb
     attr_reader :me
     alias_method :me?, :me
 
-    # @return [Integer] the ID of the emoji, if it was custom
-    attr_reader :id
+    # @return [Emoji] the emoji that was reacted with
+    attr_reader :emoji
 
-    # @return [String] the name or unicode representation of the emoji
-    attr_reader :name
-
-    def initialize(data)
+    def initialize(data, server = nil)
       @count = data['count']
       @me = data['me']
-      @id = data['emoji']['id'].nil? ? nil : data['emoji']['id'].to_i
-      @name = data['emoji']['name']
-    end
-
-    # Converts this Reaction into a string that can be sent back to Discord in other reaction endpoints.
-    # If ID is present, it will be rendered into the form of `name:id`.
-    # @return [String] the name of this reaction, including the ID if it is a custom emoji
-    def to_s
-      id.nil? ? name : "#{name}:#{id}"
+      @emoji = Emoji.new(data['emoji'], server)
     end
   end
 end
