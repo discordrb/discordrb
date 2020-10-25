764346c88d95c8b2499717c564f09a987bfd1a60
Matt Carey <matthew.b.carey@gmail.com>
2020-10-25 00:29:47 -0400

    Update URLs to use v8

--- lib/discordrb/api.rb
+++ lib/discordrb/api.rb
@@ -9,7 +9,7 @@ require 'discordrb/errors'
 # List of methods representing endpoints in Discord's API
 module Discordrb::API
   # The base URL of the Discord REST API.
-  APIBASE = 'https://discordapp.com/api/v6'
+  APIBASE = 'https://discord.com/api/v8'
 
   # The URL of Discord's CDN
   CDN_URL = 'https://cdn.discordapp.com'
